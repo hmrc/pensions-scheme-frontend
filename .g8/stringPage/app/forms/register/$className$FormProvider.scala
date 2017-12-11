c51f7fae3c89bda1e7f16bbc694a90027235a7a2
Suryabala Singh <suryabala.singh@digital.hmrc.gov.uk>
2017-12-11 14:32:02 +0000

    Add register route for the string page scaffold

--- .g8/stringPage/app/forms/register/$className$FormProvider.scala
+++ .g8/stringPage/app/forms/register/$className$FormProvider.scala
@@ -1,9 +1,10 @@
-package forms
+package forms.$routeFile$
 
 import javax.inject.Inject
 
 import forms.mappings.Mappings
 import play.api.data.Form
+import forms.FormErrorHelper
 
 class $className$FormProvider @Inject() extends FormErrorHelper with Mappings {
 
