a8a4b99de159c9f8766e5c7451d43823853f1015
Suryabala Singh <suryabala.singh@digital.hmrc.gov.uk>
2017-12-12 10:02:35 +0000

    PODS-181-Scaffold created for what you will need page

---
+++ app/controllers/register/WhatYouWillNeedController.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register
+
+import javax.inject.Inject
+
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import connectors.DataCacheConnector
+import controllers.actions._
+import config.FrontendAppConfig
+import models.Mode
+import utils.{Navigator, UserAnswers}
+import views.html.register.whatYouWillNeed
+
+import scala.concurrent.Future
+
+class WhatYouWillNeedController @Inject()(appConfig: FrontendAppConfig,
+                                         override val messagesApi: MessagesApi,
+                                         authenticate: AuthAction,
+                                         getData: DataRetrievalAction) extends FrontendController with I18nSupport {
+
+  def onPageLoad = (authenticate andThen getData) {
+    implicit request =>
+      Ok(whatYouWillNeed(appConfig))
+  }
+}
