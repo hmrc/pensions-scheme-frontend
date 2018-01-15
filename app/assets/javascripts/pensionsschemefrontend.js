ac65eae49d4a41572e577187386541cbc4482d1e
Suryabala Singh <suryabala.singh@digital.hmrc.gov.uk>
2018-01-15 16:35:45 +0000

    PODS-232-Added the javascript

--- app/assets/javascripts/pensionsschemefrontend.js
+++ app/assets/javascripts/pensionsschemefrontend.js
@@ -20,6 +20,15 @@ $(document).ready(function() {
     window.history.back();
   })
 
+  // temporary fix for IE not registering clicks on the text of the results list for the country autocomplete
+  $('body').on('mouseup', ".autocomplete__option > strong", function(e)
+  { e.preventDefault(); $(this).parent().trigger('click') }
+  )
+  // temporary fix for the autocomplete holding onto the last matching country when a user then enters an invalid or blank country
+  $('input[role="combobox"]').on('keydown', function()
+  { document.querySelector('.autocomplete-wrapper select').value = ""; }
+  )
+
     if(document.querySelectorAll('select').length > 0){
         var graphUrl = '/assets/javascripts/autocomplete/location-autocomplete-graph.json'
 
