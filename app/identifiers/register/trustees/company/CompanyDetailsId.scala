a84e3e89261faec284ea0a51a1de751b0333c8fd
Andrew Arkless <AndrewArkless@users.noreply.github.com>
2018-03-20 17:16:30 +0000

    PODS-202 Trustee Company Details added tests not passing

---
+++ app/identifiers/register/trustees/company/CompanyDetailsId.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package identifiers.register.trustees.company
+
+import identifiers._
+import identifiers.register.establishers.EstablishersId
+import identifiers.register.trustees.TrusteesId
+import models.register.trustees.company.CompanyDetails
+import play.api.libs.json.JsPath
+
+
+case class CompanyDetailsId(index:Int) extends TypedIdentifier[CompanyDetails] {
+  override def path: JsPath = TrusteesId.path \ index \ CompanyDetailsId.toString
+}
+
+object CompanyDetailsId {
+  override lazy val toString: String = "companyDetails"
+}
