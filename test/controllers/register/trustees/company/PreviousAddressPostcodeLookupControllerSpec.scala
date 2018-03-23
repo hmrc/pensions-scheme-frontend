7d1f37d52a3c6919441ec287181667f18163d6c8
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2018-03-23 15:16:18 +0000

    Share msg keys for postode lookup failure, implement onSubmit in PreviousAddressPostcodeLookupController

--- test/controllers/register/trustees/company/PreviousAddressPostcodeLookupControllerSpec.scala
+++ test/controllers/register/trustees/company/PreviousAddressPostcodeLookupControllerSpec.scala
@@ -18,21 +18,28 @@ package controllers.register.trustees.company
 
 import base.CSRFRequest
 import config.FrontendAppConfig
-import connectors.{AddressLookupConnector, DataCacheConnector}
+import connectors.{AddressLookupConnector, DataCacheConnector, FakeDataCacheConnector}
 import controllers.ControllerSpecBase
-import controllers.actions.{AuthAction, DataRetrievalAction, FakeAuthAction}
+import controllers.actions._
 import forms.address.PostcodeLookupFormProvider
+import models.address.{Address, AddressRecord}
 import models.{CompanyDetails, Index, NormalMode}
+import org.mockito.Matchers
+import org.mockito.Mockito.when
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mockito.MockitoSugar
 import play.api.data.Form
+import play.api.i18n.MessagesApi
 import play.api.inject.bind
+import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import utils.{FakeNavigator, Navigator}
 import viewmodels.Message
 import viewmodels.address.PostcodeLookupViewModel
 import views.html.address.postcodeLookup
+
+import scala.concurrent.Future
 
 class PreviousAddressPostcodeLookupControllerSpec extends ControllerSpecBase with CSRFRequest with MockitoSugar with ScalaFutures  {
 
@@ -46,18 +53,19 @@ class PreviousAddressPostcodeLookupControllerSpec extends ControllerSpecBase wit
   val companyName: String = "test company name"
   val company = CompanyDetails(companyName, None, None)
 
+  val fakeAddressLookupConnector: AddressLookupConnector = mock[AddressLookupConnector]
+
   "PreviousAddressPostcodeLookup Controller" must {
 
     "render postcodeLookup from GET request" in {
 
       val cacheConnector: DataCacheConnector = mock[DataCacheConnector]
-      val addressConnector: AddressLookupConnector = mock[AddressLookupConnector]
 
       running(_.overrides(
         bind[FrontendAppConfig].to(frontendAppConfig),
         bind[Navigator].toInstance(FakeNavigator),
         bind[DataCacheConnector].toInstance(cacheConnector),
-        bind[AddressLookupConnector].toInstance(addressConnector),
+        bind[AddressLookupConnector].toInstance(fakeAddressLookupConnector),
         bind[AuthAction].to(FakeAuthAction),
         bind[DataRetrievalAction].to(getMandatoryEstablisherCompany)
       )) {
@@ -87,6 +95,43 @@ class PreviousAddressPostcodeLookupControllerSpec extends ControllerSpecBase wit
             form,
             viewModel
           )(request, messages).toString
+      }
+    }
+
+    "redirect to next page on POST request" which {
+      "returns a list of addresses from addressLookup given a postcode" in {
+
+        val call: Call = routes.PreviousAddressPostcodeLookupController.onSubmit(NormalMode, firstIndex)
+
+        val validPostcode = "ZZ11ZZ"
+
+        when(fakeAddressLookupConnector.addressLookupByPostCode(Matchers.eq(validPostcode))(Matchers.any(), Matchers.any()))
+          .thenReturn(Future.successful(
+            Some(Seq(AddressRecord(Address("address line 1", "address line 2", None, None, Some(validPostcode), "GB"))))
+          ))
+
+        running(_.overrides(
+          bind[FrontendAppConfig].to(frontendAppConfig),
+          bind[MessagesApi].to(messagesApi),
+          bind[DataCacheConnector].toInstance(FakeDataCacheConnector),
+          bind[AddressLookupConnector].toInstance(fakeAddressLookupConnector),
+          bind[Navigator].toInstance(new FakeNavigator(desiredRoute = onwardRoute)),
+          bind[AuthAction].to(FakeAuthAction),
+          bind[DataRetrievalAction].to(getMandatoryEstablisherCompany),
+          bind[DataRequiredAction].to(new DataRequiredActionImpl),
+          bind[PostcodeLookupFormProvider].to(formProvider)
+        )) {
+          implicit app =>
+
+            val fakeRequest = addToken(FakeRequest(call)
+              .withFormUrlEncodedBody("value" -> validPostcode)
+              .withHeaders("Csrf-Token" -> "nocheck"))
+
+            val result = route(app, fakeRequest).value
+
+            status(result) must be(SEE_OTHER)
+            redirectLocation(result).value mustEqual onwardRoute.url
+        }
       }
     }
   }
