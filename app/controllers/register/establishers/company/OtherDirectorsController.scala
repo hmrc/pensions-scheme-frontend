8df7a4da49f37cebfdef89b5fc1fac3c3d96e3af
Andrew Arkless <AndrewArkless@users.noreply.github.com>
2018-04-12 13:58:38 +0100

    PODS-579 start of intial page

---
+++ app/controllers/register/establishers/company/OtherDirectorsController.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.establishers.company
+
+import javax.inject.Inject
+
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import connectors.DataCacheConnector
+import controllers.actions._
+import config.FrontendAppConfig
+import forms.register.establishers.company.OtherDirectorsFormProvider
+import identifiers.register.establishers.company.OtherDirectorsId
+import models.{Index, Mode}
+import utils.{Navigator, UserAnswers}
+import views.html.register.establishers.company.otherDirectors
+
+import scala.concurrent.Future
+
+class OtherDirectorsController @Inject() (
+                                                     appConfig: FrontendAppConfig,
+                                                     override val messagesApi: MessagesApi,
+                                                     dataCacheConnector: DataCacheConnector,
+                                                     navigator: Navigator,
+                                                     authenticate: AuthAction,
+                                                     getData: DataRetrievalAction,
+                                                     requireData: DataRequiredAction,
+                                                     formProvider: OtherDirectorsFormProvider
+                                                   ) extends FrontendController with I18nSupport {
+
+  private val form: Form[Boolean] = formProvider()
+
+  def onPageLoad(mode: Mode, establisherIndex: Index) = (authenticate andThen getData andThen requireData) {
+    implicit request =>
+      val preparedForm = request.userAnswers.get(OtherDirectorsId) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+      Ok(otherDirectors(appConfig, preparedForm, mode,establisherIndex))
+  }
+
+  def onSubmit(mode: Mode, establisherIndex: Index) = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          Future.successful(BadRequest(otherDirectors(appConfig, formWithErrors, mode,establisherIndex))),
+        (value) =>
+          dataCacheConnector.save(request.externalId, OtherDirectorsId, value).map(cacheMap =>
+            Redirect(navigator.nextPage(OtherDirectorsId, mode)(new UserAnswers(cacheMap))))
+      )
+  }
+}
