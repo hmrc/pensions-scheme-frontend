412f1250f0dbc793460fc5567bbf1a3f723ecc35
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2018-04-12 16:03:19 +0100

    Declarations - were any of companies dormant?

---
+++ app/controllers/register/DeclarationDormantController.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register
+
+import javax.inject.Inject
+
+import config.FrontendAppConfig
+import connectors.DataCacheConnector
+import controllers.Retrievals
+import controllers.actions._
+import forms.register.DeclarationDormantFormProvider
+import identifiers.register.{DeclarationDormantId, SchemeDetailsId}
+import models.Mode
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import utils.{Enumerable, Navigator, UserAnswers}
+import views.html.register.declarationDormant
+
+import scala.concurrent.Future
+
+class DeclarationDormantController @Inject()(
+                                       appConfig: FrontendAppConfig,
+                                       override val messagesApi: MessagesApi,
+                                       dataCacheConnector: DataCacheConnector,
+                                       navigator: Navigator,
+                                       authenticate: AuthAction,
+                                       getData: DataRetrievalAction,
+                                       requireData: DataRequiredAction,
+                                       formProvider: DeclarationDormantFormProvider
+                                     ) extends FrontendController with I18nSupport with Enumerable.Implicits with Retrievals {
+
+  private val form = formProvider()
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      SchemeDetailsId.retrieve.right.map{ schemeDetails =>
+        val preparedForm = request.userAnswers.get(DeclarationDormantId) match {
+          case None => form
+          case Some(value) => form.fill(value)
+        }
+        Future.successful(Ok(declarationDormant(appConfig, preparedForm, mode, schemeDetails.schemeName)))
+      }
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          SchemeDetailsId.retrieve.right.map { schemeDetails =>
+            Future.successful(BadRequest(declarationDormant(appConfig, formWithErrors, mode, schemeDetails.schemeName)))
+          },
+        (value) =>
+          dataCacheConnector.save(request.externalId, DeclarationDormantId, value).map(cacheMap =>
+            Redirect(navigator.nextPage(DeclarationDormantId, mode)(new UserAnswers(cacheMap))))
+      )
+  }
+}
---
+++ test/views/register/DeclarationDormantViewSpec.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.register
+
+import play.api.data.Form
+import forms.register.DeclarationDormantFormProvider
+import models.NormalMode
+import models.register.DeclarationDormant
+import views.behaviours.ViewBehaviours
+import views.html.register.declarationDormant
+
+class DeclarationDormantViewSpec extends ViewBehaviours {
+
+  val schemeName = "MyScheme"
+
+  val messageKeyPrefix = "declarationDormant"
+
+  val form = new DeclarationDormantFormProvider()()
+
+  def createView = () => declarationDormant(frontendAppConfig, form, NormalMode, schemeName)(fakeRequest, messages)
+
+  def createViewUsingForm = (form: Form[_]) => declarationDormant(frontendAppConfig, form, NormalMode, schemeName)(fakeRequest, messages)
+
+  "DeclarationDormant view" must {
+    behave like normalPage(createView, messageKeyPrefix, messages(s"messages__${messageKeyPrefix}__title"))
+
+    behave like pageWithBackLink(createView)
+
+    behave like pageWithSecondaryHeader(createView, schemeName)
+  }
+
+  "DeclarationDormant view" when {
+    "rendered" must {
+      "contain radio buttons for the value" in {
+        val doc = asDocument(createViewUsingForm(form))
+        for (option <- DeclarationDormant.options) {
+          assertContainsRadioButton(doc, s"value-${option.value}", "value", option.value, false)
+        }
+      }
+    }
+
+    for(option <- DeclarationDormant.options) {
+      s"rendered with a value of '${option.value}'" must {
+        s"have the '${option.value}' radio button selected" in {
+          val doc = asDocument(createViewUsingForm(form.bind(Map("value" -> s"${option.value}"))))
+          assertContainsRadioButton(doc, s"value-${option.value}", "value", option.value, true)
+
+          for(unselectedOption <- DeclarationDormant.options.filterNot(o => o == option)) {
+            assertContainsRadioButton(doc, s"value-${unselectedOption.value}", "value", unselectedOption.value, false)
+          }
+        }
+      }
+    }
+  }
+}
