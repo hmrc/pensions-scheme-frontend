f8161d899677b16cda227fd489a7cb5068bc42fb
Andrew Arkless <AndrewArkless@users.noreply.github.com>
2018-04-20 12:50:26 +0100

    PODS-821 Intial set up of lookup

---
+++ app/controllers/register/adviser/AdviserPostCodeLookupController.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.adviser
+
+import javax.inject.Inject
+
+import config.FrontendAppConfig
+import connectors.{AddressLookupConnector, DataCacheConnector}
+import controllers.actions.{AuthAction, DataRequiredAction, DataRetrievalAction}
+import controllers.address.PostcodeLookupController
+import forms.address.PostCodeLookupFormProvider
+import identifiers.register.AdviserPostcodeLookupId
+import identifiers.register.establishers.company.{CompanyDetailsId, CompanyPostCodeLookupId}
+import models.{Index, Mode}
+import play.api.data.Form
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Action, AnyContent}
+import utils.Navigator
+import viewmodels.Message
+import viewmodels.address.PostcodeLookupViewModel
+
+
+class AdviserPostCodeLookupController @Inject() (
+                                                  override val appConfig: FrontendAppConfig,
+                                                  override val messagesApi: MessagesApi,
+                                                  override val cacheConnector: DataCacheConnector,
+                                                  override val addressLookupConnector: AddressLookupConnector,
+                                                  override val navigator: Navigator,
+                                                  authenticate: AuthAction,
+                                                  getData: DataRetrievalAction,
+                                                  requireData: DataRequiredAction,
+                                                  formProvider: PostCodeLookupFormProvider
+                                                ) extends PostcodeLookupController {
+
+  private val title: Message = "messages__companyAddress__title"
+  private val heading: Message = "messages__companyAddress__heading"
+  private val invalidPostcode: Message = "messages__error__postcode_invalid"
+  private val noResults: Message = "messages__error__postcode_no_results"
+
+  protected val form: Form[String] = formProvider()
+
+  private def viewmodel(mode: Mode): Retrieval[PostcodeLookupViewModel] =
+    Retrieval {
+      implicit request =>
+        AdviserPostcodeLookupId.retrieve.right.map {
+          details =>
+            PostcodeLookupViewModel(
+              routes.AdviserPostCodeLookupController.onSubmit(mode),
+              routes.AdviserPostCodeLookupController.onPageLoad(mode),
+              title = Message(title),
+              heading = Message(heading),
+              subHeading = Some("TODO!")
+            )
+        }
+    }
+
+  def onPageLoad(mode: Mode): Action[AnyContent] =
+    (authenticate andThen getData andThen requireData).async {
+      implicit request =>
+        viewmodel(mode).retrieve.right map get
+    }
+
+  def onSubmit(mode: Mode): Action[AnyContent] =
+    (authenticate andThen getData andThen requireData).async {
+      implicit request =>
+        viewmodel(mode).retrieve.right.map {
+          vm =>
+            post(AdviserPostcodeLookupId, vm, mode)
+        }
+    }
+}
---
+++ test/controllers/register/adviser/AdviserPostcodeLookupControllerSpec.scala
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.adviser
+
+import base.CSRFRequest
+import config.FrontendAppConfig
+import connectors.{AddressLookupConnector, DataCacheConnector, FakeDataCacheConnector}
+import controllers.actions._
+import controllers.register.establishers.company.routes
+import controllers.{ControllerSpecBase, routes}
+import forms.address.PostCodeLookupFormProvider
+import models.NormalMode
+import models.address.{Address, AddressRecord}
+import org.mockito.Matchers
+import org.mockito.Mockito.when
+import org.scalatest.OptionValues
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mockito.MockitoSugar
+import play.api.i18n.Messages.Message
+import play.api.i18n.MessagesApi
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{OK, SEE_OTHER, contentAsString, redirectLocation, route, running, status}
+import uk.gov.hmrc.http.HeaderCarrier
+import viewmodels.Message
+import viewmodels.address.PostcodeLookupViewModel
+import views.html.address.postcodeLookup
+
+import scala.concurrent.Future
+
+class AdviserPostcodeLookupControllerSpec extends ControllerSpecBase with MockitoSugar with ScalaFutures with CSRFRequest with OptionValues {
+
+  def onwardRoute: Call = routes.IndexController.onPageLoad(NormalMode)
+
+  def manualInputCall: Call = routes.IndexController.onPageLoad(NormalMode)
+
+  private def fakeAddress(postCode: String) = Address(
+    "Address Line 1",
+    "Address Line 2",
+    Some("Address Line 3"),
+    Some("Address Line 4"),
+    Some(postCode),
+    "GB"
+  )
+
+  private val testAnswer = "AB12 1AB"
+
+  val formProvider = new PostCodeLookupFormProvider()
+  val form = formProvider()
+
+  val fakeAddressLookupConnector: AddressLookupConnector = mock[AddressLookupConnector]
+  implicit val hc: HeaderCarrier = mock[HeaderCarrier]
+
+  lazy val viewModel = PostcodeLookupViewModel(
+    postCall = routes.IndexController.onSubmit(NormalMode),
+    manualInputCall = manualInputCall,
+    title = Message("messages__adviserAddress__title"),
+    heading = Message("messages__adviserAddress__heading"),
+    subHeading = Some(Message("message__adviserAddress__subheading"))
+  )
+
+  "Adviser Postcode Controller" must {
+
+    "render postcodeLookop from GET request" in {
+      val cacheConnector: DataCacheConnector = mock[DataCacheConnector]
+      val addressConnector: AddressLookupConnector = mock[AddressLookupConnector]
+
+      running(_.overrides(
+        bind[FrontendAppConfig].to(frontendAppConfig),
+        bind[DataCacheConnector].toInstance(cacheConnector),
+        bind[AddressLookupConnector].toInstance(addressConnector),
+        bind[AuthAction].to(FakeAuthAction),
+        bind[DataRetrievalAction].to(getMandatoryEstablisherCompany)
+      )) {
+        implicit app =>
+
+          val request = addToken(FakeRequest(routes.AdviserPostCodeLookupController.onPageLoad(NormalMode))
+            .withHeaders("Csrf-Token" -> "nocheck"))
+
+          val result = route(app, request).value
+
+          status(result) must be(OK)
+
+          contentAsString(result) mustEqual postcodeLookup(
+            frontendAppConfig,
+            form,
+            viewModel
+          )(request, messages).toString
+      }
+    }
+
+    "redirect to next page on POST request" in {
+
+      val call: Call =routes.adviser.AdviserPostCodeLookupController.onSubmit(NormalMode)
+
+      val validPostcode = "ZZ1 1ZZ"
+
+      when(fakeAddressLookupConnector.addressLookupByPostCode(Matchers.eq(validPostcode))(Matchers.any(), Matchers.any()))
+        .thenReturn(Future.successful(
+          Some(Seq(AddressRecord(fakeAddress(testAnswer)))))
+        )
+
+      running(_.overrides(
+        bind[FrontendAppConfig].to(frontendAppConfig),
+        bind[MessagesApi].to(messagesApi),
+        bind[DataCacheConnector].toInstance(FakeDataCacheConnector),
+        bind[AddressLookupConnector].toInstance(fakeAddressLookupConnector),
+        bind[AuthAction].to(FakeAuthAction),
+        bind[DataRetrievalAction].to(getMandatoryEstablisherCompany),
+        bind[DataRequiredAction].to(new DataRequiredActionImpl),
+        bind[PostCodeLookupFormProvider].to(formProvider)
+      )) {
+        implicit app =>
+
+          val fakeRequest = addToken(FakeRequest(call)
+            .withFormUrlEncodedBody("value" -> validPostcode)
+            .withHeaders("Csrf-Token" -> "nocheck"))
+
+          val result = route(app, fakeRequest).value
+
+          status(result) must be(SEE_OTHER)
+          redirectLocation(result).value mustEqual onwardRoute.url
+
+      }
+    }
+  }
+}
