0ee152fdea5e0c7c81509846e0dac3ce1af8454e
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2018-06-26 15:21:22 +0100

    Adding functionality for each kind of establisher

--- app/controllers/register/establishers/AlreadyDeletedController.scala
+++ app/controllers/register/establishers/AlreadyDeletedController.scala
@@ -19,10 +19,16 @@ package controllers.register.establishers
 import config.FrontendAppConfig
 import controllers.Retrievals
 import controllers.actions.{AuthAction, DataRequiredAction, DataRetrievalAction}
+import identifiers.register.establishers.company.CompanyDetailsId
+import identifiers.register.establishers.individual.EstablisherDetailsId
 import javax.inject.Inject
+import models.register.establishers.EstablisherKind
+import models.register.establishers.EstablisherKind.{Company, Indivdual, Partnership}
+import models.register.establishers.individual.EstablisherDetails
+import models.requests.DataRequest
 import models.{Index, NormalMode}
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.{Action, AnyContent}
+import play.api.mvc.{Action, AnyContent, Result}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import utils.Enumerable
 import viewmodels.{AlreadyDeletedViewModel, Message}
@@ -38,16 +44,27 @@ class AlreadyDeletedController @Inject()(
                                           requireData: DataRequiredAction
                                         ) extends FrontendController with Retrievals with I18nSupport with Enumerable.Implicits {
 
-  def onPageLoad(establisherIndex: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+  def onPageLoad(index: Index, establisherKind: EstablisherKind): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
     implicit request =>
-      retrieveEstablisherName(establisherIndex)(establisher =>
-        Future.successful(Ok(alreadyDeleted(appConfig, vm(establisherIndex, establisher))))
-      )
+      establisherName(index, establisherKind) match {
+        case Right(establisherName) =>
+          Future.successful(Ok(alreadyDeleted(appConfig, vm(establisherName))))
+        case Left(result) => result
+      }
   }
 
-  private def vm(establisherIndex: Index, establisherName: String) = AlreadyDeletedViewModel(
+  private def vm(establisherName: String) = AlreadyDeletedViewModel(
     title = Message("messages__alreadyDeleted__establisher_title"),
     deletedEntity = establisherName,
     returnCall = controllers.register.establishers.routes.AddEstablisherController.onPageLoad(NormalMode)
   )
+
+  private def establisherName(index: Index, establisherKind: EstablisherKind)(implicit dataRequest: DataRequest[AnyContent]): Either[Future[Result], String] = {
+    establisherKind match {
+      case Company => CompanyDetailsId(index).retrieve.right.map(_.companyName)
+      case Indivdual => EstablisherDetailsId(index).retrieve.right.map(_.fullName)
+      case Partnership => Left(Future.successful(SeeOther(controllers.routes.SessionExpiredController.onPageLoad().url)))
+      case invalid => Left(Future.successful(SeeOther(controllers.routes.SessionExpiredController.onPageLoad().url)))
+    }
+  }
 }
