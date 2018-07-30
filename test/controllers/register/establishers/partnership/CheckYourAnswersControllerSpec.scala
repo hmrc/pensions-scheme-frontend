63bfb71cc39997c229d536fc76c0232d1aa7c423
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2018-07-30 15:31:54 +0100

    First test of partnership CYA

---
+++ test/controllers/register/establishers/partnership/CheckYourAnswersControllerSpec.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.establishers.partnership
+
+import controllers.ControllerSpecBase
+import controllers.actions.{DataRequiredActionImpl, DataRetrievalAction, FakeAuthAction}
+import identifiers.register.establishers.partnership._
+import models.AddressYears.UnderAYear
+import models.address.Address
+import models.{ContactDetails, Index, PartnershipDetails, Vat}
+import play.api.test.Helpers._
+import utils._
+
+class CheckYourAnswersControllerSpec extends ControllerSpecBase {
+
+  val firstIndex = Index(0)
+  val partnershipName = "PartnershipName"
+
+  private def controller(dataRetrievalAction: DataRetrievalAction = getEmptyData): CheckYourAnswersController =
+    new CheckYourAnswersController(
+      frontendAppConfig,
+      messagesApi,
+      FakeAuthAction,
+      dataRetrievalAction,
+      new DataRequiredActionImpl,
+      FakeSectionComplete,
+      FakeNavigator
+    )
+
+  "CheckYourAnswersController" must {
+
+    val partnershipAnswers = UserAnswers()
+      .set(PartnershipDetailsId(firstIndex))(PartnershipDetails(partnershipName))
+      .flatMap(_.set(PartnershipVatId(firstIndex))(Vat.No))
+      .flatMap(_.set(PartnershipAddressId(firstIndex))(Address("Address 1", "Address 2", None, None, None, "GB")))
+      .flatMap(_.set(PartnershipAddressYearsId(firstIndex))(UnderAYear))
+      .flatMap(_.set(PartnershipPreviousAddressId(firstIndex))(Address("Previous Address 1", "Previous Address 2", None, None, None, "US")))
+      .flatMap(_.set(PartnershipContactDetailsId(firstIndex))(ContactDetails("e@mail.co", "98765")))
+      .asOpt.value
+
+    "display answers" in {
+
+
+      val request = FakeDataRequest(partnershipAnswers)
+      val result = controller(partnershipAnswers.dataRetrievalAction).onPageLoad(firstIndex)(request)
+
+      status(result) mustBe OK
+//      contentAsString(result) mustBe viewAsString(answerSections(request))
+    }
+
+  }
+
+  "redirect to Session Expired when establisher name cannot be retrieved" in {
+
+  }
+
+}
