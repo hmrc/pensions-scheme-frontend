114d9a34c41edaa011247842f6b7d5c67da24afd
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2018-07-30 09:15:23 +0100

    Adding Partnership Details Page

---
+++ app/forms/register/establishers/partnership/PartnershipDetailsFormProvider.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.register.establishers.partnership
+
+import forms.mappings.{Mappings, PayeMapping, Transforms, VatMapping}
+import javax.inject.Inject
+import models.{CompanyDetails, PartnershipDetails}
+import play.api.data.Form
+import play.api.data.Forms.{mapping, optional}
+
+class PartnershipDetailsFormProvider  @Inject() extends Mappings with Transforms {
+
+  val partnerNameLength: Int = 160
+
+  def apply(): Form[PartnershipDetails] = Form(
+    mapping(
+      "name" -> text(errorKey = "messages__partnershipDetails__error__required")
+        .verifying(
+          firstError(
+            maxLength(
+              partnerNameLength,
+              errorKey = "messages__partnershipDetails__error_too_long"
+            ),
+            safeText(errorKey = "messages__partnershipDetails__invalid")
+          )
+        )
+    )(PartnershipDetails.applyDelete)(PartnershipDetails.unapplyDelete)
+  )
+}
---
+++ test/controllers/register/establishers/partnership/PartnershipDetailsControllerSpec.scala
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.establishers.partnership
+
+import connectors.FakeDataCacheConnector
+import controllers.ControllerSpecBase
+import controllers.actions.{DataRequiredActionImpl, DataRetrievalAction, FakeAuthAction, FakeDataRetrievalAction}
+import forms.register.establishers.partnership.PartnershipDetailsFormProvider
+import identifiers.register.SchemeDetailsId
+import identifiers.register.establishers.EstablishersId
+import identifiers.register.establishers.partnership.PartnershipDetailsId
+import models.{Index, NormalMode, PartnershipDetails}
+import models.register.{SchemeDetails, SchemeType}
+import play.api.data.Form
+import play.api.libs.json.{JsObject, Json}
+import play.api.mvc.Call
+import play.api.test.Helpers._
+import utils.FakeNavigator
+import views.html.register.establishers.partnership.partnershipDetails
+
+class PartnershipDetailsControllerSpec extends ControllerSpecBase {
+
+  def onwardRoute: Call = controllers.routes.IndexController.onPageLoad()
+
+  val formProvider = new PartnershipDetailsFormProvider()
+  val form = formProvider()
+  val firstIndex = Index(0)
+  val invalidIndex = Index(3)
+  val schemeName = "Test Scheme Name"
+
+  def controller(dataRetrievalAction: DataRetrievalAction = getMandatorySchemeName): PartnershipDetailsController =
+    new PartnershipDetailsController(frontendAppConfig, messagesApi, FakeDataCacheConnector, new FakeNavigator(desiredRoute = onwardRoute), FakeAuthAction,
+      dataRetrievalAction, new DataRequiredActionImpl, formProvider)
+
+  def viewAsString(form: Form[_] = form): String = partnershipDetails(
+    frontendAppConfig,
+    form,
+    NormalMode,
+    firstIndex,
+    schemeName
+  )(fakeRequest, messages).toString
+
+  val validData: JsObject = Json.obj(
+    SchemeDetailsId.toString ->
+      SchemeDetails(schemeName, SchemeType.SingleTrust),
+    EstablishersId.toString -> Json.arr(
+      Json.obj(
+        PartnershipDetailsId.toString ->
+          PartnershipDetails("test partnership name")
+      )
+    )
+
+  )
+
+  "CompanyDetails Controller" must {
+
+    "return OK and the correct view for a GET when scheme name is present" in {
+      val result = controller().onPageLoad(NormalMode, firstIndex)(fakeRequest)
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe viewAsString()
+    }
+
+    "populate the view correctly on a GET when the question has previously been answered" in {
+      val getRelevantData = new FakeDataRetrievalAction(Some(validData))
+
+      val result = controller(getRelevantData).onPageLoad(NormalMode, firstIndex)(fakeRequest)
+
+      contentAsString(result) mustBe viewAsString(form.fill(PartnershipDetails("test company name")))
+    }
+
+    "redirect to session expired page on a GET when the index is not valid" ignore {
+      val getRelevantData = new FakeDataRetrievalAction(Some(validData))
+      val result = controller(getRelevantData).onPageLoad(NormalMode, invalidIndex)(fakeRequest)
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+    }
+
+    "redirect to the next page when valid data is submitted" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody(("name", "test company name"))
+
+      val result = controller().onSubmit(NormalMode, firstIndex)(postRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(onwardRoute.url)
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody(("value", "invalid value"))
+      val boundForm = form.bind(Map("value" -> "invalid value"))
+
+      val result = controller().onSubmit(NormalMode, firstIndex)(postRequest)
+
+      status(result) mustBe BAD_REQUEST
+      contentAsString(result) mustBe viewAsString(boundForm)
+    }
+
+    "redirect to Session Expired for a GET if no existing data is found" in {
+      val result = controller(dontGetAnyData).onPageLoad(NormalMode, firstIndex)(fakeRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+    }
+
+    "redirect to Session Expired for a POST if no existing data is found" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody(("field1", "value 1"), ("field2", "value 2"))
+      val result = controller(dontGetAnyData).onSubmit(NormalMode, firstIndex)(postRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+    }
+  }
+}
