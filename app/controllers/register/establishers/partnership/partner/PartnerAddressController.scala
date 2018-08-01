cd7de2e26888ec70ebef3e57c82d7af21a53ade2
Madhura <madhuranakate@gmail.com>
2018-08-01 14:44:02 +0100

    PODS-287 Partner address pages

---
+++ app/controllers/register/establishers/partnership/partner/PartnerAddressController.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.establishers.partnership.partner
+
+import audit.AuditService
+import config.FrontendAppConfig
+import connectors.DataCacheConnector
+import controllers.actions._
+import controllers.address.ManualAddressController
+import controllers.register.establishers.partnership.partner.routes._
+import forms.address.AddressFormProvider
+import identifiers.register.establishers.partnership.partner.{PartnerAddressId, PartnerAddressListId, PartnerDetailsId}
+import javax.inject.Inject
+import models.address.Address
+import models.{Index, Mode}
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent}
+import utils.annotations.EstablishersPartner
+import utils.{CountryOptions, Navigator}
+import viewmodels.Message
+import viewmodels.address.ManualAddressViewModel
+
+class PartnerAddressController @Inject()(
+                                           val appConfig: FrontendAppConfig,
+                                           val messagesApi: MessagesApi,
+                                           val dataCacheConnector: DataCacheConnector,
+                                           @EstablishersPartner val navigator: Navigator,
+                                           authenticate: AuthAction,
+                                           getData: DataRetrievalAction,
+                                           requireData: DataRequiredAction,
+                                           val formProvider: AddressFormProvider,
+                                           val countryOptions: CountryOptions,
+                                           val auditService: AuditService
+                                         ) extends ManualAddressController with I18nSupport {
+
+  private[controllers] val postCall = PartnerAddressController.onSubmit _
+  private[controllers] val title: Message = "messages__partnerAddress__title"
+  private[controllers] val heading: Message = "messages__partnerAddress__heading"
+  private[controllers] val hint: Message = "messages__partnerAddress__lede"
+
+  protected val form: Form[Address] = formProvider()
+
+  def onPageLoad(mode: Mode, establisherIndex: Index, partnerIndex: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      viewmodel(establisherIndex, partnerIndex, mode).retrieve.right.map {
+        vm =>
+          get(PartnerAddressId(establisherIndex, partnerIndex), PartnerAddressListId(establisherIndex, partnerIndex), vm)
+      }
+  }
+
+  def onSubmit(mode: Mode, establisherIndex: Index, partnerIndex: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      viewmodel(establisherIndex, partnerIndex, mode).retrieve.right.map {
+        vm =>
+          post(
+            PartnerAddressId(establisherIndex, partnerIndex),
+            PartnerAddressListId(establisherIndex, partnerIndex),
+            vm,
+            mode,
+            context(vm)
+          )
+      }
+  }
+
+  private def viewmodel(establisherIndex: Int, partnerIndex: Int, mode: Mode): Retrieval[ManualAddressViewModel] =
+    Retrieval {
+      implicit request =>
+        PartnerDetailsId(establisherIndex, partnerIndex).retrieve.right.map {
+          details =>
+            ManualAddressViewModel(
+              postCall(mode, Index(establisherIndex), Index(partnerIndex)),
+              countryOptions.options,
+              title = Message(title),
+              heading = Message(heading),
+              hint = Some(Message(hint)),
+              secondaryHeader = Some(details.fullName)
+            )
+        }
+    }
+
+  private def context(viewModel: ManualAddressViewModel): String = {
+    viewModel.secondaryHeader match {
+      case Some(name) => s"Company Partner Address: $name"
+      case _ => "Company Partner Address"
+    }
+  }
+
+}
---
+++ test/controllers/register/establishers/partnership/partner/PartnerAddressListControllerSpec.scala
@@ -0,0 +1,198 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.establishers.partnership.partner
+
+import base.CSRFRequest
+import connectors.{DataCacheConnector, FakeDataCacheConnector}
+import controllers.ControllerSpecBase
+import controllers.actions.{AuthAction, DataRetrievalAction, FakeAuthAction, FakeDataRetrievalAction}
+import forms.address.AddressListFormProvider
+import identifiers.register.establishers.partnership.partner.{PartnerAddressPostcodeLookupId, PartnerDetailsId}
+import models.address.TolerantAddress
+import models.person.PersonDetails
+import models.{Index, NormalMode}
+import org.joda.time.LocalDate
+import play.api.inject.bind
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import utils.UserAnswers
+import viewmodels.Message
+import viewmodels.address.AddressListViewModel
+import views.html.address.addressList
+
+class PartnerAddressListControllerSpec extends ControllerSpecBase with CSRFRequest {
+
+  private val partnerDetails = PersonDetails("Joe", None, "Bloggs", LocalDate.now())
+
+  private val addresses = Seq(
+    TolerantAddress(
+      Some("Address 1 Line 1"),
+      Some("Address 1 Line 2"),
+      Some("Address 1 Line 3"),
+      Some("Address 1 Line 4"),
+      Some("A1 1PC"),
+      Some("GB")
+    ),
+    TolerantAddress(
+      Some("Address 2 Line 1"),
+      Some("Address 2 Line 2"),
+      Some("Address 2 Line 3"),
+      Some("Address 2 Line 4"),
+      Some("123"),
+      Some("FR")
+    )
+  )
+  private val data =
+    UserAnswers(Json.obj())
+      .set(PartnerDetailsId(0, 0))(partnerDetails)
+      .flatMap(_.set(PartnerAddressPostcodeLookupId(0, 0))(addresses))
+      .asOpt.map(_.json)
+
+  private val dataRetrievalAction = new FakeDataRetrievalAction(data)
+
+  "Company Partner Address List Controller" must {
+
+    "return Ok and the correct view on a GET request" in {
+
+      running(_.overrides(
+        bind[AuthAction].to(FakeAuthAction),
+        bind[DataCacheConnector].toInstance(FakeDataCacheConnector),
+        bind[DataRetrievalAction].toInstance(dataRetrievalAction)
+      )) { implicit app =>
+        val request = addToken(FakeRequest(routes.PartnerAddressListController.onPageLoad(NormalMode, Index(0), Index(0))))
+        val result = route(app, request).value
+
+        status(result) mustBe OK
+
+        val viewModel: AddressListViewModel = addressListViewModel(addresses)
+        val form = new AddressListFormProvider()(viewModel.addresses)
+
+        contentAsString(result) mustBe addressList(frontendAppConfig, form, viewModel)(request, messages).toString
+      }
+
+    }
+
+    "redirect to Company Address Post Code Lookup if no address data on a GET request" in {
+
+      running(_.overrides(
+        bind[AuthAction].to(FakeAuthAction),
+        bind[DataCacheConnector].toInstance(FakeDataCacheConnector),
+        bind[DataRetrievalAction].toInstance(getEmptyData)
+      )) { implicit app =>
+        val request = addToken(FakeRequest(routes.PartnerAddressListController.onPageLoad(NormalMode, Index(0), Index(0))))
+        val result = route(app, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe Some(routes.PartnerAddressPostcodeLookupController.onPageLoad(NormalMode, Index(0), Index(0)).url)
+      }
+
+    }
+
+    "redirect to Session Expired controller when no session data exists on a GET request" in {
+
+      running(_.overrides(
+        bind[AuthAction].to(FakeAuthAction),
+        bind[DataCacheConnector].toInstance(FakeDataCacheConnector),
+        bind[DataRetrievalAction].toInstance(dontGetAnyData)
+      )) { implicit app =>
+        val request = addToken(FakeRequest(routes.PartnerAddressListController.onPageLoad(NormalMode, Index(0), Index(0))))
+        val result = route(app, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+      }
+
+    }
+
+    "redirect to the next page on POST of valid data" in {
+
+      val onwardCall = controllers.routes.IndexController.onPageLoad()
+
+      running(_.overrides(
+        bind[AuthAction].to(FakeAuthAction),
+        bind[DataCacheConnector].toInstance(FakeDataCacheConnector),
+        bind[DataRetrievalAction].toInstance(dataRetrievalAction)
+      )) { implicit app =>
+        val request =
+          addToken(
+            FakeRequest(routes.PartnerAddressListController.onSubmit(NormalMode, Index(0), Index(0)))
+              .withFormUrlEncodedBody(("value", "0"))
+          )
+
+        val result = route(app, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe Some(onwardCall.url)
+      }
+
+    }
+
+    "redirect to Session Expired controller when no session data exists on a POST request" in {
+
+      running(_.overrides(
+        bind[AuthAction].to(FakeAuthAction),
+        bind[DataCacheConnector].toInstance(FakeDataCacheConnector),
+        bind[DataRetrievalAction].toInstance(dontGetAnyData)
+      )) { implicit app =>
+        val request =
+          addToken(
+            FakeRequest(routes.PartnerAddressListController.onSubmit(NormalMode, Index(0), Index(0)))
+              .withFormUrlEncodedBody(("value", "0"))
+          )
+
+        val result = route(app, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+      }
+
+    }
+
+    "redirect to Company Address Post Code Lookup if no address data on a POST request" in {
+
+      running(_.overrides(
+        bind[AuthAction].to(FakeAuthAction),
+        bind[DataCacheConnector].toInstance(FakeDataCacheConnector),
+        bind[DataRetrievalAction].toInstance(getEmptyData)
+      )) { implicit app =>
+        val request =
+          addToken(
+            FakeRequest(routes.PartnerAddressListController.onSubmit(NormalMode, Index(0), Index(0)))
+              .withFormUrlEncodedBody(("value", "0"))
+          )
+
+        val result = route(app, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe Some(routes.PartnerAddressPostcodeLookupController.onPageLoad(NormalMode, Index(0), Index(0)).url)
+      }
+
+    }
+
+  }
+
+  private def addressListViewModel(addresses: Seq[TolerantAddress]): AddressListViewModel = {
+    AddressListViewModel(
+      routes.PartnerAddressListController.onSubmit(NormalMode, Index(0), Index(0)),
+      routes.PartnerAddressController.onPageLoad(NormalMode, Index(0), Index(0)),
+      addresses,
+      subHeading = Some(Message(partnerDetails.fullName))
+    )
+  }
+
+}
