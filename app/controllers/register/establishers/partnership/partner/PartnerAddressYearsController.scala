4dead755f952d2c4c963e4ac67925f49454802ed
Madhura <madhuranakate@gmail.com>
2018-08-01 15:22:44 +0100

    PODS-288 Partner Address Years

---
+++ app/controllers/register/establishers/partnership/partner/PartnerAddressYearsController.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.establishers.partnership.partner
+
+import config.FrontendAppConfig
+import connectors.DataCacheConnector
+import controllers.Retrievals
+import controllers.actions._
+import controllers.address.AddressYearsController
+import forms.address.AddressYearsFormProvider
+import identifiers.register.establishers.partnership.partner.{PartnerAddressYearsId, PartnerDetailsId}
+import javax.inject.Inject
+import models.{Index, Mode}
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Action, AnyContent}
+import utils.Navigator
+import utils.annotations.EstablishersPartner
+import viewmodels.Message
+import viewmodels.address.AddressYearsViewModel
+
+class PartnerAddressYearsController @Inject()(
+                                                val appConfig: FrontendAppConfig,
+                                                val cacheConnector: DataCacheConnector,
+                                                @EstablishersPartner val navigator: Navigator,
+                                                val messagesApi: MessagesApi,
+                                                authenticate: AuthAction,
+                                                getData: DataRetrievalAction,
+                                                requireData: DataRequiredAction
+                                              ) extends AddressYearsController with Retrievals {
+
+  private val form = new AddressYearsFormProvider()(Message("messages__common_error__current_address_years"))
+
+  def onPageLoad(mode: Mode, establisherIndex: Index, partnerIndex: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      PartnerDetailsId(establisherIndex, partnerIndex).retrieve.right.map { partnerDetails =>
+        get(PartnerAddressYearsId(establisherIndex, partnerIndex), form, viewModel(mode, establisherIndex, partnerIndex, partnerDetails.fullName))
+      }
+  }
+
+  def onSubmit(mode: Mode, establisherIndex: Index, partnerIndex: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      PartnerDetailsId(establisherIndex, partnerIndex).retrieve.right.map { partnerDetails =>
+        post(
+          PartnerAddressYearsId(establisherIndex, partnerIndex),
+          mode,
+          form,
+          viewModel(mode, establisherIndex, partnerIndex, partnerDetails.fullName)
+        )
+      }
+  }
+
+  private def viewModel(mode: Mode, establisherIndex: Index, partnerIndex: Index, partnerName: String) = AddressYearsViewModel(
+    postCall = routes.PartnerAddressYearsController.onSubmit(mode, establisherIndex, partnerIndex),
+    title = Message("messages__partner_address_years__title"),
+    heading = Message("messages__partner_address_years__heading"),
+    legend = Message("messages__partner_address_years__heading"),
+    subHeading = Some(Message(partnerName))
+  )
+}
