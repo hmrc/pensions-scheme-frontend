d666c45cec853781c440d95efbacdde38188a58b
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2018-08-03 14:07:09 +0100

    Adding clean up to partner journey

---
+++ test/identifiers/register/establishers/partnership/partner/PartnerAddressYearsIdSpec.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package identifiers.register.establishers.partnership.partner
+
+import models.AddressYears
+import models.address.TolerantAddress
+import org.scalatest.{Matchers, OptionValues, WordSpec}
+import utils.UserAnswers
+
+class PartnerAddressYearsIdSpec extends WordSpec with Matchers with OptionValues {
+
+  import PartnerAddressYearsIdSpec._
+
+  "PartnerAddressYearsId cleanup" when {
+
+    "AddressYears is set to 'over a year'" must {
+      "remove the data for PartnerPreviousAddressPostcodeLookupId" in {
+        val newAnswers = userAnswers.set(addressYearsId)(AddressYears.OverAYear).asOpt.value
+        newAnswers.get(previousAddressPostcodeLookupId) shouldNot be(defined)
+      }
+
+      "remove the data for PartnerPreviousAddressId" in {
+        val newAnswers = userAnswers.set(addressYearsId)(AddressYears.OverAYear).asOpt.value
+        newAnswers.get(previousAddressId) shouldNot be(defined)
+      }
+    }
+
+    "AddressYears is set to 'under a year'" must {
+      "not remove the data for PartnerPreviousAddressPostcodeLookupId" in {
+        val newAnswers = userAnswers.set(addressYearsId)(AddressYears.UnderAYear).asOpt.value
+        newAnswers.get(previousAddressPostcodeLookupId) should be(defined)
+      }
+
+      "not remove the data for PartnerPreviousAddressId" in {
+        val newAnswers = userAnswers.set(addressYearsId)(AddressYears.UnderAYear).asOpt.value
+        newAnswers.get(previousAddressId) should be(defined)
+      }
+    }
+
+    "AddressYears is removed" must {
+      "not remove the data for PartnerPreviousAddressPostcodeLookupId" in {
+        val newAnswers = userAnswers.remove(addressYearsId).asOpt.value
+        newAnswers.get(previousAddressPostcodeLookupId) should be(defined)
+      }
+
+      "not remove the data for PartnerPreviousAddressId" in {
+        val newAnswers = userAnswers.remove(addressYearsId).asOpt.value
+        newAnswers.get(previousAddressId) should be(defined)
+      }
+    }
+  }
+
+}
+
+object PartnerAddressYearsIdSpec extends OptionValues {
+
+  private val establisherIndex = 0
+  private val partnerIndex = 0
+
+  private val previousAddressPostcodeLookupId = PartnerPreviousAddressPostcodeLookupId(establisherIndex, partnerIndex)
+  private val previousAddressId = PartnerPreviousAddressId(establisherIndex, partnerIndex)
+  private val addressYearsId = PartnerAddressYearsId(establisherIndex, partnerIndex)
+
+  private val address = TolerantAddress(
+    Some("test-address-line1"),
+    Some("test-address-line2"),
+    None,
+    None,
+    None,
+    Some("test-country")
+  )
+
+  private val userAnswers = UserAnswers()
+    .set(addressYearsId)(AddressYears.UnderAYear)
+    .flatMap(_.set(previousAddressPostcodeLookupId)(Seq(address)))
+    .flatMap(_.set(previousAddressId)(address.toAddress))
+    .asOpt.value
+
+}
