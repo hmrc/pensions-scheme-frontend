d235ad85c678c2f348d2cea1f62212bf9446be81
Suryabala Singh <suryabala.singh@digital.hmrc.gov.uk>
2018-08-06 12:33:35 +0100

    PODS-301-Partnership Navigator is in progress

--- app/navigators/EstablishersPartnershipNavigator.scala
+++ app/navigators/EstablishersPartnershipNavigator.scala
@@ -19,15 +19,56 @@ package navigators
 import com.google.inject.Inject
 import config.FrontendAppConfig
 import connectors.DataCacheConnector
-import utils.Navigator
+import identifiers.register.establishers.partnership._
+import models.{AddressYears, NormalMode}
+import utils.{Navigator, UserAnswers}
+import controllers.register.establishers.partnership._
 
 class EstablishersPartnershipNavigator  @Inject()(val dataCacheConnector: DataCacheConnector, appConfig: FrontendAppConfig) extends Navigator {
-
+  //scalastyle:off cyclomatic.complexity
   override protected def routeMap(from: NavigateFrom): Option[NavigateTo] = from.id match {
-    case _ => None
+    case PartnershipDetailsId(index) =>
+      NavigateTo.save(routes.PartnershipVatController.onPageLoad(NormalMode, index))
+    case PartnershipVatId(index) =>
+      NavigateTo.save(routes.PartnershipPayeController.onPageLoad(NormalMode, index))
+    case PartnershipPayeId(index) =>
+      NavigateTo.save(routes.PartnershipUniqueTaxReferenceController.onPageLoad(NormalMode, index))
+    case PartnershipUniqueTaxReferenceID(index) =>
+      NavigateTo.save(routes.PartnershipPostcodeLookupController.onPageLoad(NormalMode, index))
+    case PartnershipPostcodeLookupId(index) =>
+      NavigateTo.save(routes.PartnershipAddressListController.onPageLoad(NormalMode, index))
+    case PartnershipAddressListId(index) =>
+      NavigateTo.save(routes.PartnershipAddressController.onPageLoad(NormalMode, index))
+    case PartnershipAddressId(index) =>
+      NavigateTo.save(routes.PartnershipAddressYearsController.onPageLoad(NormalMode, index))
+    case PartnershipAddressYearsId(index) =>
+      addressYearsRoutes(index)(from.userAnswers)
+    case PartnershipPreviousAddressPostcodeLookupId(index) =>
+      NavigateTo.save(routes.PartnershipPreviousAddressListController.onPageLoad(NormalMode, index))
+    case PartnershipPreviousAddressListId(index) =>
+      NavigateTo.save(routes.PartnershipPreviousAddressController.onPageLoad(NormalMode, index))
+    case PartnershipPreviousAddressId(index) =>
+      NavigateTo.save(routes.PartnershipContactDetailsController.onPageLoad(NormalMode, index))
+    case PartnershipContactDetailsId(index) =>
+      NavigateTo.save(routes.CheckYourAnswersController.onPageLoad(index))
+    case CheckYourAnswersId(index) =>
+      NavigateTo.save(controllers.register.establishers.partnership.routes.AddPartnersController.onPageLoad(index))
+    case OtherPartnersId(index) =>
+      
   }
 
   override protected def editRouteMap(from: NavigateFrom): Option[NavigateTo] = from.id match {
     case _ => None
+  }
+
+  private def addressYearsRoutes(index: Int)(answers: UserAnswers): Option[NavigateTo] = {
+    answers.get(PartnershipAddressYearsId(index)) match {
+      case Some(AddressYears.UnderAYear) =>
+        NavigateTo.save(routes.PartnershipPreviousAddressPostcodeLookupController.onPageLoad(NormalMode, index))
+      case Some(AddressYears.OverAYear) =>
+        NavigateTo.save(routes.PartnershipContactDetailsController.onPageLoad(NormalMode, index))
+      case None =>
+        NavigateTo.dontSave(controllers.routes.SessionExpiredController.onPageLoad())
+    }
   }
 }
