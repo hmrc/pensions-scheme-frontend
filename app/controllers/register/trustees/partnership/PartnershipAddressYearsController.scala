014adbcd861e5a177b63e5cfad37cb3654c2566e
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2018-08-07 16:12:22 +0100

    Adding address years page

--- app/controllers/register/trustees/partnership/PartnershipAddressYearsController.scala
+++ app/controllers/register/trustees/partnership/PartnershipAddressYearsController.scala
@@ -16,6 +16,54 @@
 
 package controllers.register.trustees.partnership
 
-class PartnershipAddressYearsController {
+import com.google.inject.Inject
+import config.FrontendAppConfig
+import connectors.DataCacheConnector
+import controllers.Retrievals
+import controllers.actions.{AuthAction, DataRequiredAction, DataRetrievalAction}
+import controllers.address.AddressYearsController
+import forms.address.AddressYearsFormProvider
+import identifiers.register.trustees.partnership.{PartnershipAddressYearsId, PartnershipDetailsId}
+import models.{Index, Mode}
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Action, AnyContent}
+import utils.Navigator
+import utils.annotations.TrusteesPartnership
+import viewmodels.Message
+import viewmodels.address.AddressYearsViewModel
+
+class PartnershipAddressYearsController @Inject()(
+                                                   override val appConfig: FrontendAppConfig,
+                                                   override val cacheConnector: DataCacheConnector,
+                                                   @TrusteesPartnership override val navigator: Navigator,
+                                                   override val messagesApi: MessagesApi,
+                                                   authenticate: AuthAction,
+                                                   getData: DataRetrievalAction,
+                                                   requireData: DataRequiredAction
+                                                 ) extends AddressYearsController with Retrievals {
+
+  private val form = new AddressYearsFormProvider()(Message("messages__partnershipAddressYears__error"))
+
+  def onPageLoad(mode: Mode, index: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      PartnershipDetailsId(index).retrieve.right.map { partnershipDetails =>
+        get(PartnershipAddressYearsId(index), form, viewModel(mode, index, partnershipDetails.name))
+      }
+  }
+
+  def onSubmit(mode: Mode, index: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      PartnershipDetailsId(index).retrieve.right.map { partnershipDetails =>
+        post(PartnershipAddressYearsId(index), mode, form, viewModel(mode, index, partnershipDetails.name))
+      }
+  }
+
+  private def viewModel(mode: Mode, index: Index, partnershipName: String) = AddressYearsViewModel(
+    postCall = routes.PartnershipAddressYearsController.onSubmit(mode, index),
+    title = Message("messages__partnershipAddressYears__title"),
+    heading = Message("messages__partnershipAddressYears__heading"),
+    legend = Message("messages__partnershipAddressYears__heading"),
+    subHeading = Some(Message(partnershipName))
+  )
 
 }
--- test/controllers/register/trustees/partnership/PartnershipAddressYearsControllerSpec.scala
+++ test/controllers/register/trustees/partnership/PartnershipAddressYearsControllerSpec.scala
@@ -16,6 +16,150 @@
 
 package controllers.register.trustees.partnership
 
-class PartnershipAddressYearsControllerSpec {
+import connectors.FakeDataCacheConnector
+import controllers.ControllerSpecBase
+import controllers.actions.{DataRequiredActionImpl, DataRetrievalAction, FakeAuthAction, FakeDataRetrievalAction}
+import forms.address.AddressYearsFormProvider
+import identifiers.register.trustees.partnership.{PartnershipAddressYearsId, PartnershipDetailsId}
+import models.{AddressYears, Index, NormalMode, PartnershipDetails}
+import models._
+import play.api.data.Form
+import play.api.libs.json._
+import play.api.test.Helpers._
+import utils.{FakeNavigator, UserAnswers}
+import viewmodels.Message
+import viewmodels.address.AddressYearsViewModel
+import views.html.address.addressYears
+
+class PartnershipAddressYearsControllerSpec extends ControllerSpecBase {
+
+  import PartnershipAddressYearsControllerSpec._
+
+  "TrusteeAddressYearsController" must {
+
+    "return OK and the correct view on a GET request" in {
+      val result = controller(trusteeData).onPageLoad(mode, index)(fakeRequest)
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe viewAsString()
+    }
+
+    "populate the view correctly on a GET when the question has previously been answered" in {
+      val answer = AddressYears.OverAYear
+      val filledForm = form.fill(answer)
+      assume(filledForm.errors.isEmpty)
+
+      val result = controller(partnershipAndAnswerData(answer)).onPageLoad(mode, index)(fakeRequest)
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe viewAsString(filledForm)
+    }
+
+    "redirect to Session Expired on a GET request if no cached data exists" in {
+      val result = controller(dontGetAnyData).onPageLoad(mode, index)(fakeRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+    }
+
+    "redirect to the next page when valid data is submitted" in {
+      val answer = AddressYears.values.head
+      val request = fakeRequest.withFormUrlEncodedBody(("value", answer.toString))
+
+      val result = controller(trusteeData).onSubmit(mode, index)(request)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(onwardRoute.url)
+    }
+
+    "save the answer when valid data is submitted" in {
+      val answer = AddressYears.values.head
+      val request = fakeRequest.withFormUrlEncodedBody(("value", answer.toString))
+
+      val result = controller(trusteeData).onSubmit(mode, index)(request)
+
+      status(result) mustBe SEE_OTHER
+      FakeDataCacheConnector.verify(PartnershipAddressYearsId(index), answer)
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+      val filledForm = form.bind(Map.empty[String, String])
+      assume(filledForm.errors.nonEmpty)
+
+      val result = controller(trusteeData).onSubmit(mode, index)(fakeRequest)
+
+      status(result) mustBe BAD_REQUEST
+      contentAsString(result) mustBe viewAsString(filledForm)
+    }
+
+    "redirect to Session Expired on a POST request if no cached data exists" in {
+      val result = controller(dontGetAnyData).onSubmit(mode, index)(fakeRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+    }
+
+  }
+
+}
+
+object PartnershipAddressYearsControllerSpec extends ControllerSpecBase {
+
+  private val mode = NormalMode
+  private val index = Index(0)
+
+  private val partnership = PartnershipDetails(
+    "test partnership name"
+  )
+
+  private val form = new AddressYearsFormProvider()(Message("messages__partnershipAddressYears__error"))
+
+  private val onwardRoute = controllers.routes.IndexController.onPageLoad()
+  private val fakeNavigator = new FakeNavigator(onwardRoute)
+
+  private def controller(dataRetrievalAction: DataRetrievalAction) =
+    new PartnershipAddressYearsController(
+      frontendAppConfig,
+      FakeDataCacheConnector,
+      fakeNavigator,
+      messagesApi,
+      FakeAuthAction,
+      dataRetrievalAction,
+      new DataRequiredActionImpl()
+    )
+
+  private val viewModel =
+    AddressYearsViewModel(
+      postCall = routes.PartnershipAddressYearsController.onSubmit(mode, index),
+      title = Message("messages__partnershipAddressYears__title"),
+      heading = Message("messages__partnershipAddressYears__heading"),
+      legend = Message("messages__partnershipAddressYears__heading"),
+      subHeading = Some(Message(partnership.name))
+    )
+
+  private def viewAsString(form: Form[AddressYears] = form) =
+    addressYears(
+      frontendAppConfig,
+      form,
+      viewModel
+    )(fakeRequest, messages).toString()
+
+  private def partnershipUserAnswers: UserAnswers = {
+    UserAnswers().set(PartnershipDetailsId(index))(partnership) match {
+      case JsSuccess(userAnswers, _) => userAnswers
+      case JsError(errors) => throw JsResultException(errors)
+    }
+  }
+
+  private def trusteeData: DataRetrievalAction = {
+    new FakeDataRetrievalAction(Some(partnershipUserAnswers.json))
+  }
+
+  private def partnershipAndAnswerData(answer: AddressYears): DataRetrievalAction = {
+    partnershipUserAnswers.set(PartnershipAddressYearsId(index))(answer) match {
+      case JsSuccess(userAnswers, _) => new FakeDataRetrievalAction(Some(userAnswers.json))
+      case JsError(errors) => throw JsResultException(errors)
+    }
+  }
 
 }
