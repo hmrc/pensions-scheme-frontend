939d18cdd938a6ed99d8c7bc0c0d2fec5301013b
madhuranakate <madhuranakate@gmail.com>
2018-11-27 15:06:11 +0000

    Page and tests for IscompanyDormant

---
+++ app/models/register/establishers/Dormancy.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.register.establishers
+
+import play.api.libs.json.Json
+import play.api.mvc.JavascriptLiteral
+import utils.{Enumerable, InputOption, WithName}
+
+sealed trait Dormancy
+
+object Dormancy {
+
+  case object Dormant extends WithName("yes") with Dormancy
+
+  case object NotDormant extends WithName("no") with Dormancy
+
+  val values: Seq[Dormancy] = Seq(
+    Dormant, NotDormant
+  )
+
+  val options: Seq[InputOption] = values.map {
+    value =>
+      InputOption(value.toString, s"messages__is_dormant__${value.toString}")
+  }
+
+  implicit val enumerable: Enumerable[Dormancy] =
+    Enumerable(values.map(v => v.toString -> v): _*)
+}
