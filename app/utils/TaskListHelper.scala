895eb215a5d3de825922d5adcf25ffb99384b4f1
madhuranakate <madhuranakate@gmail.com>
2018-11-28 16:45:48 +0000

    Abstracted code to helper

---
+++ app/utils/TaskListHelper.scala
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import identifiers.register.IsAboutSchemeCompleteId
+import identifiers.register.adviser.IsWorkingKnowledgeCompleteId
+import identifiers.register.establishers.company.{CompanyDetailsId => EstablisherCompanyDetailsId}
+import identifiers.register.establishers.individual.EstablisherDetailsId
+import identifiers.register.establishers.partnership.{PartnershipDetailsId => EstablisherPartnershipDetailsId}
+import identifiers.register.trustees.company.{CompanyDetailsId => TrusteeCompanyDetailsId}
+import identifiers.register.trustees.individual.TrusteeDetailsId
+import identifiers.register.trustees.partnership.{PartnershipDetailsId => TrusteePartnershipDetailsId}
+import models.register.Entity
+import models.{JourneyTaskList, JourneyTaskListSection, Link, NormalMode}
+import play.api.i18n.Messages
+
+class TaskListHelper(journey: UserAnswers)(implicit messages: Messages) {
+
+  def tasklist: JourneyTaskList = JourneyTaskList(
+    aboutSection,
+    establishers,
+    trustees,
+    workingKnowledgeSection,
+    declarationLink)
+
+  private def aboutSection = JourneyTaskListSection(
+    journey.get(IsAboutSchemeCompleteId),
+    Link(messages("messages__schemeTaskList__about_link_text"),
+      controllers.register.routes.SchemeDetailsController.onPageLoad(NormalMode).url),
+    None)
+
+  private def workingKnowledgeSection = JourneyTaskListSection(
+    journey.get(IsWorkingKnowledgeCompleteId),
+    Link(messages("messages__schemeTaskList__working_knowledge_add_link"),
+      controllers.register.routes.SchemeDetailsController.onPageLoad(NormalMode).url),
+    None)
+
+  private def declarationLink: Option[Link] =
+    if(declarationEnabled)
+      Some(Link(messages("messages__schemeTaskList__declaration_link"),
+        controllers.register.routes.DeclarationController.onPageLoad().url))
+    else None
+
+  private def declarationEnabled: Boolean =
+    (journey.get(IsAboutSchemeCompleteId), journey.get(IsWorkingKnowledgeCompleteId)) match {
+      case (Some(true), Some(true)) if journey.allEstablishersAfterDelete.forall(_.isCompleted) &&
+        (journey.allTrusteesAfterDelete.forall(_.isCompleted)) => true
+      case _ => false
+    }
+
+  private def establishers:Seq[JourneyTaskListSection] = {
+    val establishers = journey.allEstablishersAfterDelete
+    if(establishers.isEmpty)
+      Seq(JourneyTaskListSection(
+        None,
+        Link(messages("messages__schemeTaskList__establishers_add_link"),
+          controllers.register.establishers.routes.EstablisherKindController.onPageLoad(NormalMode, journey.establishersCount).url),
+        None))
+    else
+      listOfSections(establishers)
+
+  }
+
+  private def trustees:Seq[JourneyTaskListSection] = {
+    val trustees = journey.allTrusteesAfterDelete
+    if(trustees.isEmpty)
+      Seq(JourneyTaskListSection(
+        None,
+        Link(messages("messages__schemeTaskList__trustees_add_link"),
+          controllers.register.trustees.routes.TrusteeKindController.onPageLoad(NormalMode, journey.trusteesCount).url),
+        None))
+    else
+      listOfSections(trustees)
+
+  }
+
+  private def listOfSections(sections: Seq[Entity[_]]): Seq[JourneyTaskListSection] =
+    for(section <- sections) yield
+      JourneyTaskListSection(
+        Some(section.isCompleted),
+        Link(linkText(section),
+          section.editLink),
+        Some(section.name))
+
+  private def linkText(item: Entity[_]): String =
+    item.id match {
+      case EstablisherCompanyDetailsId(_) | TrusteeCompanyDetailsId(_) => "Company details"
+      case EstablisherDetailsId(_) | TrusteeDetailsId(_) => "Individual details"
+      case EstablisherPartnershipDetailsId(_) | TrusteePartnershipDetailsId(_) => "Partnership details"
+    }
+}
