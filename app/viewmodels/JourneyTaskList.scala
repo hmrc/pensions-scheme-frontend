a8b1b2c27ec1f48226c0020208cb7e4147112d34
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2018-11-29 08:30:29 +0000

    PODS-1999 Restore declaration section to model

--- app/viewmodels/JourneyTaskList.scala
+++ app/viewmodels/JourneyTaskList.scala
@@ -14,6 +14,23 @@
  * limitations under the License.
  */
 
+
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package viewmodels
 
 import play.api.libs.json.{Json, OFormat}
@@ -22,10 +39,8 @@ case class JourneyTaskList(about: JourneyTaskListSection,
                            establishers: Seq[JourneyTaskListSection],
                            trustees: Seq[JourneyTaskListSection],
                            workingKnowledge: JourneyTaskListSection,
-                           declaration: JourneyTaskListSection){
-  def allSectionsCompleted: Boolean = {
-    true
-  }
+                           declaration: Option[Link] = None){
+
 }
 
 object JourneyTaskList {
@@ -40,7 +55,6 @@ case class JourneyTaskListSection(isCompleted: Option[Boolean] = None, link: Lin
       case _ => None
     }
 }
-
 
 case class Link(text: String, target: String)
 
