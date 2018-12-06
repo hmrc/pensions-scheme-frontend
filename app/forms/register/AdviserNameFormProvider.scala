f2e75ed593326f9d17b583827eaba8ec4ca78ce7
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2018-12-06 12:47:04 +0000

    PODS-1944 Add error message

--- app/forms/register/AdviserNameFormProvider.scala
+++ app/forms/register/AdviserNameFormProvider.scala
@@ -24,7 +24,7 @@ class AdviserNameFormProvider @Inject() extends Mappings with Constraints {
   val schemeNameMaxLength = 160
 
   def apply(): Form[String] = Form(
-    "adviserName" -> text("???").
+    "adviserName" -> text("messages__adviserName__blank").
       verifying(firstError(
         maxLength(schemeNameMaxLength, "messages__adviserName__length"),
         safeText("messages__adviserName__invalid")))
