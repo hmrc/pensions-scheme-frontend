eb67678bb52ae015222c819bd736697de7213349
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2018-12-17 10:04:58 +0000

    PODS-1612 Create page, controller and tests

---
+++ test/views/BeforeYouStartViewSpec.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import config.FrontendAppConfig
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.twirl.api.HtmlFormat
+import views.behaviours.ViewBehaviours
+import views.html.beforeYouStart
+
+class BeforeYouStartViewSpec extends ViewBehaviours {
+
+  override def frontendAppConfig: FrontendAppConfig = new GuiceApplicationBuilder().configure(
+    conf = "features.is-hub-enabled" -> false
+  ).build().injector.instanceOf[FrontendAppConfig]
+
+  val messageKeyPrefix = "what_you_will_need"
+
+  def createView: () => HtmlFormat.Appendable = () => beforeYouStart(frontendAppConfig)(fakeRequest, messages)
+
+  "BeforeYouStart view" must {
+
+    behave like normalPage(createView, messageKeyPrefix, messages(s"messages__${messageKeyPrefix}__title"),
+      "_lede", "_item_1", "_item_2", "_item_3", "_item_4")
+
+    behave like pageWithSubmitButton(createView)
+  }
+}
+
+class BeforeYouStartHsViewSpec extends ViewBehaviours {
+
+  override def frontendAppConfig: FrontendAppConfig = new GuiceApplicationBuilder().configure(
+    conf = "features.is-hub-enabled" -> true
+  ).build().injector.instanceOf[FrontendAppConfig]
+
+  val messageKeyPrefix = "hs_what_you_will_need"
+
+  def createView: () => HtmlFormat.Appendable = () => beforeYouStart(frontendAppConfig)(fakeRequest, messages)
+
+  "BeforeYouStart view (hub and spoke version)" must {
+
+    behave like normalPage(createView, messageKeyPrefix, messages(s"messages__${messageKeyPrefix}__title"),
+      "_lede", "_p2", "_item_1", "_item_2", "_item_3", "_item_4", "_p3")
+
+    behave like pageWithSubmitButton(createView)
+
+    "have a link to the register a scheme page on gov uk" in {
+      val doc = asDocument(createView())
+      assertLink(doc, linkId = "apply-to-register-govuk-link", url = frontendAppConfig.applyToRegisterLink)
+    }
+
+    "have a link to the managing pension schemes" in {
+      val doc = asDocument(createView())
+      assertLink(doc, linkId = "return-managing", url = frontendAppConfig.managePensionsSchemeOverviewUrl.url)
+    }
+  }
+}
