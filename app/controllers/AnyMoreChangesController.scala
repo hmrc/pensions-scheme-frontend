36244cbce394a610491f1364bfd58206acc77884
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-01-22 10:18:41 +0000

    PODS-2186 Create new page etc

---
+++ app/controllers/AnyMoreChangesController.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.FrontendAppConfig
+import connectors.UserAnswersCacheConnector
+import controllers.actions._
+import forms.AnyMoreChangesFormProvider
+import identifiers.AnyMoreChangesId
+import javax.inject.Inject
+import models.NormalMode
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import utils.annotations.Variations
+import utils.{Navigator, UserAnswers}
+import views.html.anyMoreChanges
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class AnyMoreChangesController @Inject()(appConfig: FrontendAppConfig,
+                                         override val messagesApi: MessagesApi,
+                                         dataCacheConnector: UserAnswersCacheConnector,
+                                         @Variations navigator: Navigator,
+                                         authenticate: AuthAction,
+                                         getData: DataRetrievalAction,
+                                         requireData: DataRequiredAction,
+                                         formProvider: AnyMoreChangesFormProvider)(implicit val ec: ExecutionContext)
+  extends FrontendController with Retrievals with I18nSupport {
+
+  private val form: Form[Boolean] = formProvider()
+
+  def onPageLoad: Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      Future.successful(Ok(anyMoreChanges(appConfig, form)))
+  }
+
+  def onSubmit: Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          Future.successful(BadRequest(anyMoreChanges(appConfig, formWithErrors))),
+        value =>
+          dataCacheConnector.save(request.externalId, AnyMoreChangesId, value).map(cacheMap =>
+            Redirect(navigator.nextPage(AnyMoreChangesId, NormalMode, UserAnswers(cacheMap))))
+      )
+  }
+}
+
---
+++ app/navigators/VariationsNavigator.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package navigators
+
+import com.google.inject.Inject
+import config.FrontendAppConfig
+import connectors.UserAnswersCacheConnector
+import identifiers.HaveAnyTrusteesId
+import identifiers.register.SchemeDetailsId
+import identifiers.register.establishers.{AddEstablisherId, ConfirmDeleteEstablisherId, EstablisherKindId}
+import models.NormalMode
+import models.register.establishers.EstablisherKind
+import models.register.{SchemeDetails, SchemeType}
+import utils.{Enumerable, Navigator, UserAnswers}
+
+class VariationsNavigator @Inject()(val dataCacheConnector: UserAnswersCacheConnector,
+                                    config: FrontendAppConfig)extends Navigator with Enumerable.Implicits {
+
+  override protected def routeMap(from: NavigateFrom): Option[NavigateTo] =
+    from.id match {
+      case AddEstablisherId(value) => addEstablisherRoutes(value, from.userAnswers)
+      case EstablisherKindId(index) => establisherKindRoutes(index, from.userAnswers)
+      case ConfirmDeleteEstablisherId =>
+        NavigateTo.save(controllers.register.establishers.routes.AddEstablisherController.onPageLoad(NormalMode))
+      case _ => None
+    }
+
+  override protected def editRouteMap(from: NavigateFrom): Option[NavigateTo] = None
+
+  private def addEstablisherRoutes(value: Option[Boolean], answers: UserAnswers): Option[NavigateTo] = {
+    value match {
+      case Some(false) =>
+        navigateToTaskList()
+      case Some(true) =>
+        NavigateTo.save(controllers.register.establishers.routes.EstablisherKindController.onPageLoad(NormalMode, answers.establishersCount))
+      case None =>
+        NavigateTo.save(controllers.register.establishers.routes.EstablisherKindController.onPageLoad(NormalMode, answers.establishersCount))
+    }
+  }
+
+  private def navigateBasedOnSchemeDetails(answers: UserAnswers): Option[NavigateTo] = {
+    val listOfSchemeTypeTrusts: Seq[SchemeType] = Seq(SchemeType.SingleTrust, SchemeType.MasterTrust)
+    answers.get(SchemeDetailsId) match {
+      case Some(SchemeDetails(_, schemeType)) if listOfSchemeTypeTrusts.contains(schemeType) =>
+        NavigateTo.save(controllers.register.trustees.routes.AddTrusteeController.onPageLoad(NormalMode))
+      case Some(SchemeDetails(_, _)) =>
+        answers.allTrusteesAfterDelete.isEmpty match {
+          case false =>
+            NavigateTo.save(controllers.register.trustees.routes.AddTrusteeController.onPageLoad(NormalMode))
+          case _ =>
+            answers.get(HaveAnyTrusteesId) match {
+              case Some(true) =>
+                NavigateTo.save(controllers.register.trustees.routes.AddTrusteeController.onPageLoad(NormalMode))
+              case Some(false) =>
+                NavigateTo.save(controllers.register.routes.SchemeReviewController.onPageLoad())
+              case _ =>
+                NavigateTo.save(controllers.register.trustees.routes.HaveAnyTrusteesController.onPageLoad(NormalMode))
+            }
+        }
+      case None =>
+        NavigateTo.dontSave(controllers.routes.SessionExpiredController.onPageLoad())
+    }
+  }
+
+  private def establisherKindRoutes(index: Int, answers: UserAnswers): Option[NavigateTo] = {
+    answers.get(EstablisherKindId(index)) match {
+      case Some(EstablisherKind.Company) =>
+        NavigateTo.save(controllers.register.establishers.company.routes.CompanyDetailsController.onPageLoad(NormalMode, index))
+      case Some(EstablisherKind.Indivdual) =>
+        NavigateTo.save(controllers.register.establishers.individual.routes.EstablisherDetailsController.onPageLoad(NormalMode, index))
+      case Some(EstablisherKind.Partnership) =>
+        NavigateTo.save(controllers.register.establishers.partnership.routes.PartnershipDetailsController.onPageLoad(NormalMode, index))
+      case _ =>
+        NavigateTo.dontSave(controllers.routes.SessionExpiredController.onPageLoad())
+    }
+  }
+
+  private def navigateToTaskList() =
+    if(config.enableHubV2)
+      NavigateTo.dontSave(controllers.routes.SchemeTaskListController.onPageLoad())
+    else
+      NavigateTo.dontSave(controllers.register.routes.SchemeTaskListController.onPageLoad())
+}
