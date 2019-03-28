d19d19bfd70405b93139e2d3b89143fa5e66a762
Madhura <madhuranakate@gmail.com>
2019-03-28 15:55:50 +0000

    PODS-2234 Add date to any more changes page

--- test/views/vary/AnyMoreChangesViewSpec.scala
+++ test/views/vary/AnyMoreChangesViewSpec.scala
@@ -18,31 +18,37 @@ package views.vary
 
 import controllers.vary.routes
 import forms.vary.AnyMoreChangesFormProvider
+import org.jsoup.Jsoup
 import play.api.data.Form
 import play.twirl.api.HtmlFormat
+import viewmodels.Message
 import views.behaviours.YesNoViewBehaviours
 import views.html.vary.anyMoreChanges
 
 class AnyMoreChangesViewSpec extends YesNoViewBehaviours {
   val schemeName = Some("Scheme x")
   val messageKeyPrefix = "any_more_changes"
+  val date: String = "27 February 2019"
 
   val form = new AnyMoreChangesFormProvider()()
 
   def createView: () => HtmlFormat.Appendable = () =>
-    anyMoreChanges(frontendAppConfig, form, schemeName)(fakeRequest, messages)
+    anyMoreChanges(frontendAppConfig, form, schemeName, date)(fakeRequest, messages)
 
   def createViewInCheckMode: () => HtmlFormat.Appendable = () =>
-    anyMoreChanges(frontendAppConfig, form, schemeName)(fakeRequest, messages)
+    anyMoreChanges(frontendAppConfig, form, schemeName, date)(fakeRequest, messages)
 
   def createViewUsingForm: Form[_] => HtmlFormat.Appendable = (form: Form[_]) =>
-    anyMoreChanges(frontendAppConfig, form, schemeName)(fakeRequest, messages)
+    anyMoreChanges(frontendAppConfig, form, schemeName, date)(fakeRequest, messages)
 
   "Any More Changes view" must {
 
     behave like normalPage(createView, messageKeyPrefix, pageHeader = messages(s"messages__${messageKeyPrefix}__title"),
       expectedGuidanceKeys = "_p1", "_p2")
 
+    "display partnership name" in {
+      Jsoup.parse(createView().toString) must haveDynamicText(Message("messages__any_more_changes__p3_date", date))
+    }
     behave like yesNoPage(
       createView = createViewUsingForm,
       messageKeyPrefix = messageKeyPrefix,
