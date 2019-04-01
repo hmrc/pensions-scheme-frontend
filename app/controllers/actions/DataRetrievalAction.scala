c839afdb49c5281a6ea91728ea0c6a7ab9590867
Madhura <madhuranakate@gmail.com>
2019-04-01 16:31:46 +0100

    Added and fixed tests

--- app/controllers/actions/DataRetrievalAction.scala
+++ app/controllers/actions/DataRetrievalAction.scala
@@ -18,7 +18,7 @@ package controllers.actions
 
 
 import com.google.inject.{ImplementedBy, Inject}
-import connectors.{PensionSchemeVarianceLockConnector, SubscriptionCacheConnector, UpdateSchemeCacheConnector, UserAnswersCacheConnector}
+import connectors._
 import models._
 import models.requests.{AuthenticatedRequest, OptionalDataRequest}
 import play.api.libs.json.JsValue
@@ -31,11 +31,12 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class DataRetrievalImpl(dataConnector: UserAnswersCacheConnector,
-                        viewConnector: SubscriptionCacheConnector,
+                        viewConnector: SchemeDetailsReadOnlyCacheConnector,
                         updateConnector: UpdateSchemeCacheConnector,
                         lockConnector: PensionSchemeVarianceLockConnector,
-                        mode: Mode = NormalMode,
-                        srn: Option[String] = None) extends DataRetrieval {
+                        mode: Mode,
+                        srn: Option[String],
+                        disableLock: Boolean) extends DataRetrieval {
 
   override protected def transform[A](request: AuthenticatedRequest[A]): Future[OptionalDataRequest[A]] = {
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromHeadersAndSession(request.headers, Some(request.session))
@@ -46,46 +47,52 @@ class DataRetrievalImpl(dataConnector: UserAnswersCacheConnector,
         srn.map { srn =>
           lockConnector.getLock(request.psaId.id, srn).flatMap {
             case Some(_) => getOptionalRequest(updateConnector.fetch(srn))(request)
-            case _ => placeLockAndGetRequest(srn)(request, implicitly)
+            case None if disableLock => getOptionalRequest(viewConnector.fetch(request.externalId))(request)
+            case None => placeLockAndGetRequest(srn)(request, implicitly)
           }
-        }.getOrElse(Future(OptionalDataRequest(request.request, request.externalId, None, request.psaId)))
+        }.getOrElse(Future(emptyDataRequest(request)))
     }
   }
 
   def placeLockAndGetRequest[A](srn: String)(implicit request: AuthenticatedRequest[A], hc: HeaderCarrier): Future[OptionalDataRequest[A]] =
     viewConnector.fetch(request.externalId).flatMap {
-      case None => Future(OptionalDataRequest(request.request, request.externalId, None, request.psaId))
       case Some(data) =>
         lockConnector.lock(request.psaId.id, srn).flatMap {
-          case VarianceLock => updateConnector.upsert(srn, UserAnswers(data).json).map { _ =>
-            OptionalDataRequest(request.request, request.externalId, Some(UserAnswers(data)), request.psaId)
-          }
-          case _ => getOptionalRequest(viewConnector.fetch(request.externalId))(request)
+          case VarianceLock => updateConnector.upsert(srn, UserAnswers(data).json).map(_ => validDataRequest(data))
+          case _ => Future(validDataRequest(data))
         }
+      case None => Future(emptyDataRequest)
     }
+
+
+
 
   def getOptionalRequest[A](f: Future[Option[JsValue]])(implicit request: AuthenticatedRequest[A]): Future[OptionalDataRequest[A]] =
     f.map {
-      case None => OptionalDataRequest(request.request, request.externalId, None, request.psaId)
-      case Some(data) => OptionalDataRequest(request.request, request.externalId, Some(UserAnswers(data)), request.psaId)
-
+      case None => emptyDataRequest
+      case Some(data) => validDataRequest(data)
     }
+
+  private def emptyDataRequest[A](implicit request: AuthenticatedRequest[A]) =
+    OptionalDataRequest(request.request, request.externalId, None, request.psaId)
+
+  private def validDataRequest[A](data:JsValue)(implicit request: AuthenticatedRequest[A]) =
+    OptionalDataRequest(request.request, request.externalId, Some(UserAnswers(data)), request.psaId)
 }
 
 @ImplementedBy(classOf[DataRetrievalImpl])
 trait DataRetrieval extends ActionTransformer[AuthenticatedRequest, OptionalDataRequest]
 
 class DataRetrievalActionImpl @Inject()(dataConnector: UserAnswersCacheConnector,
-                                        viewConnector: SubscriptionCacheConnector,
+                                        viewConnector: SchemeDetailsReadOnlyCacheConnector,
                                         updateConnector: UpdateSchemeCacheConnector,
                                         lockConnector: PensionSchemeVarianceLockConnector
                                        ) extends DataRetrievalAction {
-  override def apply(mode: Mode = NormalMode,
-                     srn: Option[String] = None): DataRetrieval =
-    new DataRetrievalImpl(dataConnector, viewConnector, updateConnector, lockConnector, mode, srn)
+  override def apply(mode: Mode, srn: Option[String], disableLock: Boolean): DataRetrieval =
+    new DataRetrievalImpl(dataConnector, viewConnector, updateConnector, lockConnector, mode, srn, disableLock)
 }
 
 @ImplementedBy(classOf[DataRetrievalActionImpl])
 trait DataRetrievalAction {
-  def apply(mode: Mode = NormalMode, srn: Option[String] = None): DataRetrieval
+  def apply(mode: Mode = NormalMode, srn: Option[String] = None, disableLock: Boolean = false): DataRetrieval
 }
