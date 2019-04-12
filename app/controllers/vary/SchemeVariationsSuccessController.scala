cd27e4a71cbea251ae6041946e47d2b120fcb2d8
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-04-12 09:10:36 +0100

    PODS-2195 Add new page

---
+++ app/controllers/vary/SchemeVariationsSuccessController.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.vary
+
+import config.FrontendAppConfig
+import connectors.UserAnswersCacheConnector
+import controllers.Retrievals
+import controllers.actions._
+import javax.inject.Inject
+import models.UpdateMode
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import views.html.vary.schemeVariationsSuccess
+
+import scala.concurrent.ExecutionContext
+
+class SchemeVariationsSuccessController @Inject()(appConfig: FrontendAppConfig,
+                                                  override val messagesApi: MessagesApi,
+                                                  cacheConnector: UserAnswersCacheConnector,
+                                                  authenticate: AuthAction,
+                                                  getData: DataRetrievalAction)
+                                                 (implicit val ec: ExecutionContext) extends FrontendController with I18nSupport with Retrievals {
+
+  def onPageLoad(srn: String): Action[AnyContent] = (authenticate andThen getData(UpdateMode, Some(srn))).async {
+    implicit request =>
+      val schemeName = existingSchemeName
+      cacheConnector.removeAll(request.externalId).map { _ =>
+        Ok(
+          schemeVariationsSuccess(
+            appConfig, schemeName, Some(srn)
+          )
+        )
+      }
+  }
+}
