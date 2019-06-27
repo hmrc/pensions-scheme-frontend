f95847b4cf9cb2b1e3a81ef876e7882e909ec6d5
Chris I'Anson <chris.ianson@digital.hmrc.gov.uk>
2019-06-27 09:14:24 +0100

    PODS-1899: Refactor NoCompanyNumberControllerSpec

--- test/controllers/register/establishers/company/NoCompanyNumberControllerSpec.scala
+++ test/controllers/register/establishers/company/NoCompanyNumberControllerSpec.scala
@@ -26,8 +26,10 @@ import models.NormalMode
 import models.requests.DataRequest
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{MustMatchers, OptionValues, WordSpec}
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.i18n.MessagesApi
 import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.mvc.{AnyContent, Call, Request, Result}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
@@ -39,115 +41,72 @@ import views.html.reason
 
 import scala.concurrent.Future
 
-class NoCompanyNumberControllerSpec extends WordSpec with MustMatchers with OptionValues with ScalaFutures {
+class NoCompanyNumberControllerSpec extends WordSpec with MustMatchers with OptionValues with ScalaFutures with GuiceOneAppPerSuite {
 
   import NoCompanyNumberControllerSpec._
 
+  override lazy val app = new GuiceApplicationBuilder()
+    .overrides(
+    bind[Navigator].toInstance(FakeNavigator),
+    bind[UserAnswersService].toInstance(FakeUserAnswersService)
+  ).build()
+
+  implicit val materializer: Materializer = app.materializer
+
+  val appConfig = app.injector.instanceOf[FrontendAppConfig]
+  val formProvider = app.injector.instanceOf[NoCompanyNumberFormProvider]
+  val request = FakeRequest()
+  val messages = app.injector.instanceOf[MessagesApi].preferred(request)
+  val controller = app.injector.instanceOf[TestController]
+
   "NoCompanyNumberController" when {
 
-    "get" must {
+    "calling get method" must {
 
       "return a successful result when there is no existing answer" in {
-
-        running(_.overrides(
-          bind[Navigator].toInstance(FakeNavigator)
-        )) {
-          app =>
-
-            implicit val materializer: Materializer = app.materializer
-
-            val appConfig = app.injector.instanceOf[FrontendAppConfig]
-            val formProvider = app.injector.instanceOf[NoCompanyNumberFormProvider]
-            val request = FakeRequest()
-            val messages = app.injector.instanceOf[MessagesApi].preferred(request)
-            val controller = app.injector.instanceOf[TestController]
-            val result = controller.onPageLoad(viewModel, UserAnswers())
-
-            status(result) mustEqual OK
-            contentAsString(result) mustEqual reason(appConfig, formProvider(companyName)(messages), viewModel, None)(request, messages).toString
-        }
+          val result = controller.onPageLoad(viewModel, UserAnswers())
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual reason(
+            appConfig,
+            formProvider("test company")(messages),
+            viewModel,
+            None)(request, messages).toString
       }
 
       "return a successful result when there is an existing answer" in {
-
-        running(_.overrides(
-          bind[Navigator].toInstance(FakeNavigator)
-        )) {
-          app =>
-
-            implicit val materializer: Materializer = app.materializer
-
-            val appConfig = app.injector.instanceOf[FrontendAppConfig]
-            val formProvider = app.injector.instanceOf[NoCompanyNumberFormProvider]
-            val request = FakeRequest()
-            val messages = app.injector.instanceOf[MessagesApi].preferred(request)
-            val controller = app.injector.instanceOf[TestController]
-            val answers = UserAnswers().set(FakeIdentifier)("123456789").get
-            val result = controller.onPageLoad(viewModel, answers)
-
-            status(result) mustEqual OK
-            contentAsString(result) mustEqual reason(
-              appConfig,
-              formProvider(companyName)(messages).fill("123456789"),
-              viewModel,
-              None
-            )(request, messages).toString
-        }
+          val answers = UserAnswers().set(FakeIdentifier)("123456789").get
+          val result = controller.onPageLoad(viewModel, answers)
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual reason(
+            appConfig,
+            formProvider("test company")(messages).fill("123456789"),
+            viewModel,
+            None
+          )(request, messages).toString
       }
+
     }
 
-    "post" must {
+    "calling post method" must {
 
       "return a redirect when the submitted data is valid" in {
-
-        import play.api.inject._
-
-        running(_.overrides(
-          bind[UserAnswersService].toInstance(FakeUserAnswersService),
-          bind[Navigator].toInstance(FakeNavigator)
-        )) {
-          app =>
-
-            implicit val materializer: Materializer = app.materializer
-
-            val request = FakeRequest().withFormUrlEncodedBody(
-              ("reason", "123456789")
-            )
-            val controller = app.injector.instanceOf[TestController]
-            val result = controller.onSubmit(viewModel, UserAnswers(), request)
-
-            status(result) mustEqual SEE_OTHER
-            redirectLocation(result).value mustEqual "www.example.com"
-            FakeUserAnswersService.verify(FakeIdentifier, "123456789")
-        }
+          val request = FakeRequest().withFormUrlEncodedBody(("reason", "123456789"))
+          val result = controller.onSubmit(viewModel, UserAnswers(), request)
+          status(result) mustEqual SEE_OTHER
+          redirectLocation(result).value mustEqual "www.example.com"
+          FakeUserAnswersService.verify(FakeIdentifier, "123456789")
       }
 
       "return a bad request when the submitted data is invalid" in {
-
-        running(_.overrides(
-          bind[Navigator].toInstance(FakeNavigator)
-        )) {
-          app =>
-
-            implicit val materializer: Materializer = app.materializer
-
-            val appConfig = app.injector.instanceOf[FrontendAppConfig]
-            val formProvider = app.injector.instanceOf[NoCompanyNumberFormProvider]
-            val controller = app.injector.instanceOf[TestController]
-            val request = FakeRequest().withFormUrlEncodedBody(("reason", "123456789{0}12345"))
-
-            val messages = app.injector.instanceOf[MessagesApi].preferred(request)
-
-            val result = controller.onSubmit(viewModel, UserAnswers(), request)
-
-            status(result) mustEqual BAD_REQUEST
-            contentAsString(result) mustEqual reason(
-              appConfig,
-              formProvider(companyName)(messages).bind(Map("reason" -> "123456789{0}12345")),
-              viewModel,
-              None
-            )(request, messages).toString
-        }
+          val request = FakeRequest().withFormUrlEncodedBody(("reason", "123456789{0}12345"))
+          val result = controller.onSubmit(viewModel, UserAnswers(), request)
+          status(result) mustEqual BAD_REQUEST
+          contentAsString(result) mustEqual reason(
+            appConfig,
+            formProvider("test company")(messages).bind(Map("reason" -> "123456789{0}12345")),
+            viewModel,
+            None
+          )(request, messages).toString
       }
     }
   }
@@ -157,10 +116,6 @@ class NoCompanyNumberControllerSpec extends WordSpec with MustMatchers with Opti
 object NoCompanyNumberControllerSpec extends NoCompanyNumberControllerSpec {
 
   object FakeIdentifier extends TypedIdentifier[String]
-
-  val companyName = "test company"
-
-  val postCall = routes.NoCompanyNumberController.onSubmit _
 
   val viewModel = ReasonViewModel(
     postCall = Call("GET", "www.example.com"),
@@ -177,11 +132,11 @@ object NoCompanyNumberControllerSpec extends NoCompanyNumberControllerSpec {
                                 ) extends ReasonController {
 
     def onPageLoad(viewModel: ReasonViewModel, answers: UserAnswers): Future[Result] = {
-      get(FakeIdentifier, viewModel, formProvider(companyName))(DataRequest(FakeRequest(), "cacheId", answers, PsaId("A0000000")))
+      get(FakeIdentifier, viewModel, formProvider("test company"))(DataRequest(FakeRequest(), "cacheId", answers, PsaId("A0000000")))
     }
 
     def onSubmit(viewModel: ReasonViewModel, answers: UserAnswers, fakeRequest: Request[AnyContent]): Future[Result] = {
-      post(FakeIdentifier, NormalMode, viewModel, formProvider(companyName))(DataRequest(fakeRequest, "cacheId", answers, PsaId("A0000000")))
+      post(FakeIdentifier, NormalMode, viewModel, formProvider("test company"))(DataRequest(fakeRequest, "cacheId", answers, PsaId("A0000000")))
     }
   }
 }
