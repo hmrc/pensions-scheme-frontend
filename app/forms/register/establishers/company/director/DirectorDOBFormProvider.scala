3a8c4ec951867050226a3f077909b21909f64443
Gorkem Unsal <gorkem.unsal@accenture.com>
2019-07-08 16:04:08 +0100

    PODS-2857 created view and spec file

---
+++ app/forms/register/establishers/company/director/DirectorDOBFormProvider.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.register.establishers.company.director
+
+import forms.mappings.{Mappings, Transforms}
+import javax.inject.Inject
+import models.person.DateOfBirth
+import play.api.data.Form
+import play.api.data.Forms._
+
+class DirectorDOBFormProvider @Inject() extends Mappings with Transforms {
+
+  def apply(): Form[DateOfBirth] = Form(
+    mapping(
+      "date" -> dateMapping("messages__error__date", "error.invalid_date")
+        .verifying(firstError(futureDate("messages__error__date_future"),
+          notBeforeYear("messages__error__date_past", DirectorDOBFormProvider.startYear)
+        )
+        )
+    )(DateOfBirth.applyDelete)(DateOfBirth.unapplyDelete)
+  )
+}
+
+object DirectorDOBFormProvider {
+  val firstNameLength: Int = 35
+  val middleNameLength: Int = 35
+  val lastNameLength: Int = 35
+  val startYear: Int = 1900
+}
