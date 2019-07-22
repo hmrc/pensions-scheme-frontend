2a2e99054396340d5469236f04ad26a8c6eeac8d
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-07-22 08:56:54 +0100

    PODS-3043 Fix unit test

--- test/controllers/register/trustees/company/CompanyAddressYearsControllerSpec.scala
+++ test/controllers/register/trustees/company/CompanyAddressYearsControllerSpec.scala
@@ -42,16 +42,17 @@ class CompanyAddressYearsControllerSpec extends ControllerSpecBase {
   val form = formProvider(Message("messages__common_error__current_address_years", companyDetails.companyName))
   val firstIndex = Index(0)
   val invalidIndex = Index(10)
-  val questionText = "messages__company_address_years__title"
+  val questionTextTitle = "messages__company_trustee_address_years__title"
+  val questionTextHeading = "messages__company_trustee_address_years__heading"
   val retrieval = new FakeDataRetrievalAction(Some(Json.obj(
     TrusteesId.toString -> Json.arr(Json.obj(CompanyDetailsId.toString -> companyDetails))
   )))
 
   val viewmodel = AddressYearsViewModel(
     postCall = routes.CompanyAddressYearsController.onSubmit(NormalMode, firstIndex, None),
-    title = Message(questionText, Message("messages__common__address_years__company").resolve),
-    heading = Message(questionText, companyDetails.companyName),
-    legend = Message(questionText, companyDetails.companyName),
+    title = Message(questionTextTitle),
+    heading = Message(questionTextHeading, companyDetails.companyName),
+    legend = Message("messages__company_address_years__title"),
     Some(companyDetails.companyName)
   )
 
