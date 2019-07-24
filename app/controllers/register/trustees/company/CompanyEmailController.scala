447e724fd5bec29cca3855445fd35d59fbab9a25
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-07-24 11:24:04 +0100

    Created page for Email address

--- app/controllers/register/trustees/company/CompanyEmailController.scala
+++ app/controllers/register/trustees/company/CompanyEmailController.scala
@@ -17,37 +17,65 @@
 package controllers.register.trustees.company
 
 import config.FrontendAppConfig
-import controllers.Retrievals
+import controllers.EmailAddressController
 import controllers.actions._
+import forms.EmailFormProvider
+import identifiers.register.trustees.company.{CompanyDetailsId, CompanyEmailId, IsContactDetailsCompleteId}
 import javax.inject.Inject
 import models.{Index, Mode}
+import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent}
 import services.UserAnswersService
-import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import utils.Navigator
 import utils.annotations.TrusteesCompany
-import utils.{Enumerable, Navigator}
+import viewmodels.{CommonFormWithHintViewModel, Message}
 
 import scala.concurrent.ExecutionContext
 
-class CompanyEmailController @Inject()(
-                                          appConfig: FrontendAppConfig,
-                                          override val messagesApi: MessagesApi,
-                                          userAnswersService: UserAnswersService,
-                                          @TrusteesCompany navigator: Navigator,
-                                          authenticate: AuthAction,
-                                          getData: DataRetrievalAction,
-                                          allowAccess: AllowAccessActionProvider,
-                                          requireData: DataRequiredAction
-                                        )(implicit val ec: ExecutionContext) extends FrontendController with Retrievals with I18nSupport with Enumerable.Implicits {
+class CompanyEmailController @Inject()(val appConfig: FrontendAppConfig,
+                                       override val messagesApi: MessagesApi,
+                                       authenticate: AuthAction,
+                                       getData: DataRetrievalAction,
+                                       override val userAnswersService: UserAnswersService,
+                                       allowAccess: AllowAccessActionProvider,
+                                       requireData: DataRequiredAction,
+                                       @TrusteesCompany val navigator: Navigator,
+                                       formProvider: EmailFormProvider
+                                      )(implicit val ec: ExecutionContext) extends EmailAddressController with I18nSupport {
+
+  protected val form: Form[String] = formProvider()
+
+  private def viewModel(mode: Mode, srn: Option[String], index: Index): Retrieval[CommonFormWithHintViewModel] =
+    Retrieval {
+      implicit request =>
+        CompanyDetailsId(index).retrieve.right.map {
+          details =>
+            CommonFormWithHintViewModel(
+              controllers.register.trustees.company.routes.CompanyEmailController.onSubmit(mode,index,srn),
+              Message("messages__establisher_email__title"),
+              Message("messages__common_email__heading", details.companyName),
+              Some(Message("messages__establisher_email__hint")),
+              srn = srn
+            )
+        }
+    }
 
   def onPageLoad(mode: Mode, index: Index, srn: Option[String]): Action[AnyContent] =
-    (authenticate andThen getData(mode, srn) andThen allowAccess(srn) andThen requireData) {
-    implicit request => Ok
-  }
+    (authenticate andThen getData(mode, srn) andThen allowAccess(srn) andThen requireData).async {
+      implicit request =>
+        viewModel(mode, srn, index).retrieve.right.map {
+          vm =>
+            get(CompanyEmailId(index), form, vm)
+        }
+    }
 
-  def onSubmit(mode: Mode, index: Index, srn: Option[String]): Action[AnyContent] = (authenticate andThen getData(mode, srn) andThen requireData) {
-    implicit request => Redirect(controllers.routes.IndexController.onPageLoad())
+  def onSubmit(mode: Mode, index: Index, srn: Option[String]): Action[AnyContent] =
+    (authenticate andThen getData(mode, srn) andThen allowAccess(srn) andThen requireData).async {
+    implicit request =>
+      viewModel(mode, srn, index).retrieve.right.map {
+        vm =>
+          post(CompanyEmailId(index), mode, form, vm, Some(IsContactDetailsCompleteId(index)))
+      }
   }
-
 }
