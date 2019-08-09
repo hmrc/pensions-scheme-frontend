206f4068abd3fa060ace79046410d52fe52f8cb8
Madhura <madhuranakate@gmail.com>
2019-08-09 16:29:32 +0100

    Fixed test

--- app/controllers/register/trustees/individual/TrusteeHasUTRController.scala
+++ app/controllers/register/trustees/individual/TrusteeHasUTRController.scala
@@ -46,7 +46,7 @@ class TrusteeHasUTRController @Inject()(val appConfig: FrontendAppConfig,
   private def viewModel(mode: Mode, index: Index, srn: Option[String], trusteeName: String): CommonFormWithHintViewModel =
     CommonFormWithHintViewModel(
       postCall = controllers.register.trustees.individual.routes.TrusteeHasUTRController.onSubmit(mode, index, srn),
-      title = Message("messages__hasUtr__h1", Message("messages__theTrustee")),
+      title = Message("messages__hasUtr__h1", Message("messages__theTrustee").resolve),
       heading = Message("messages__hasUtr__h1", trusteeName),
       hint = Some(Message("messages__hasUtr__p1")),
       srn = srn
