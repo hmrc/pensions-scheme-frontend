8305c4c8c6042b5622c72aa6f44a87d0a977e1b7
Vincent Vinnicombe <->
2019-08-15 15:47:14 +0100

    PODS-2958: Adding UpdateMode routes, wip

--- app/navigators/trustees/individuals/TrusteesIndividualAddressNavigator.scala
+++ app/navigators/trustees/individuals/TrusteesIndividualAddressNavigator.scala
@@ -20,7 +20,7 @@ import com.google.inject.Inject
 import connectors.UserAnswersCacheConnector
 import controllers.register.trustees.individual.routes._
 import controllers.routes._
-import identifiers.Identifier
+import identifiers.{AnyMoreChangesId, Identifier}
 import identifiers.register.trustees.individual._
 import models.AddressYears._
 import models._
@@ -36,10 +36,21 @@ class TrusteesIndividualAddressNavigator @Inject()(val dataCacheConnector: UserA
     case IndividualPostCodeLookupId(index)                => IndividualAddressListController.onPageLoad(mode, index, None)
     case IndividualAddressListId(index)                   => TrusteeAddressController.onPageLoad(mode, index, None)
     case TrusteeAddressId(index)                          => TrusteeAddressYearsController.onPageLoad(mode, index, None)
-    case TrusteeAddressYearsId(index)                     => trusteeAddressYearsRoutes(mode, ua, index)
+    case TrusteeAddressYearsId(index)                     => trusteeAddressYearsRoutes(mode, ua, index, None)
     case IndividualPreviousAddressPostCodeLookupId(index) => TrusteePreviousAddressListController.onPageLoad(mode, index, None)
     case TrusteePreviousAddressListId(index)              => TrusteePreviousAddressController.onPageLoad(mode, index, None)
     case TrusteePreviousAddressId(index)                  => CheckYourAnswersIndividualAddressController.onPageLoad(mode, index, None)
+  }
+
+  private def updateModeRoutes(mode: VarianceMode, ua: UserAnswers, srn: Option[String]): PartialFunction[Identifier, Call] = {
+    case IndividualPostCodeLookupId(index)                => IndividualAddressListController.onPageLoad(mode, index, srn)
+    case IndividualAddressListId(index)                   => TrusteeAddressController.onPageLoad(mode, index, srn)
+    case TrusteeAddressId(index)                          => TrusteeAddressYearsController.onPageLoad(mode, index, srn)
+    case TrusteeAddressYearsId(index)                     => trusteeAddressYearsRoutes(mode, ua, index, srn)
+    case IndividualPreviousAddressPostCodeLookupId(index) => TrusteePreviousAddressListController.onPageLoad(mode, index, srn)
+    case TrusteePreviousAddressListId(index)              => TrusteePreviousAddressController.onPageLoad(mode, index, srn)
+    case TrusteePreviousAddressId(_)                      => AnyMoreChangesController.onPageLoad(srn)
+    case AnyMoreChangesId                                 => anyMoreChanges(ua)
   }
 
   override protected def routeMap(from: NavigateFrom): Option[NavigateTo] =
@@ -48,16 +59,25 @@ class TrusteesIndividualAddressNavigator @Inject()(val dataCacheConnector: UserA
   override protected def editRouteMap(from: NavigateFrom): Option[NavigateTo] =
     navigateOrSessionReset(normalAndCheckModeRoutes(CheckMode, from.userAnswers, None), from.id)
 
-  override protected def updateRouteMap(from: NavigateFrom, srn: Option[String]): Option[NavigateTo] = ???
+  override protected def updateRouteMap(from: NavigateFrom, srn: Option[String]): Option[NavigateTo] =
+    navigateOrSessionReset(updateModeRoutes(UpdateMode, from.userAnswers, srn), from.id)
 
   override protected def checkUpdateRouteMap(from: NavigateFrom, srn: Option[String]): Option[NavigateTo] = ???
 }
 
 object TrusteesIndividualAddressNavigator {
-  private def trusteeAddressYearsRoutes(mode: Mode, ua: UserAnswers, index: Int): Call =
+  private def trusteeAddressYearsRoutes(mode: Mode, ua: UserAnswers, index: Int, srn: Option[String]): Call =
     ua.get(TrusteeAddressYearsId(index)) match {
-      case Some(AddressYears.OverAYear) => CheckYourAnswersIndividualAddressController.onPageLoad(mode, index, None)
-      case Some(AddressYears.UnderAYear) => IndividualPreviousAddressPostcodeLookupController.onPageLoad(mode, index, None)
+      case Some(AddressYears.OverAYear) => CheckYourAnswersIndividualAddressController.onPageLoad(mode, index, srn)
+      case Some(AddressYears.UnderAYear) => IndividualPreviousAddressPostcodeLookupController.onPageLoad(mode, index, srn)
       case _ => SessionExpiredController.onPageLoad()
+    }
+
+  private def anyMoreChanges(ua: UserAnswers): Call =
+    ua.get(AnyMoreChangesId) match {
+      case Some(true) => ???
+      case Some(false) => ???
+      case _ => SessionExpiredController.onPageLoad()
+
     }
 }
--- test/navigators/trustees/individuals/TrusteesIndividualAddressNavigatorSpec.scala
+++ test/navigators/trustees/individuals/TrusteesIndividualAddressNavigatorSpec.scala
@@ -18,6 +18,7 @@ package navigators.trustees.individuals
 
 import base.SpecBase
 import controllers.register.trustees.individual.routes._
+import controllers.routes.AnyMoreChangesController
 import generators.Generators
 import identifiers.Identifier
 import identifiers.register.trustees.IsTrusteeNewId
@@ -36,7 +37,7 @@ class TrusteesIndividualAddressNavigatorSpec extends SpecBase with MustMatchers 
   val navigator: Navigator = injector.instanceOf[TrusteesIndividualAddressNavigator]
 
   "NormalMode" must {
-    def navigationForNewTrusteeIndividual(mode: Mode): TableFor3[Identifier, UserAnswers, Call] =
+    def normalModeRoutes(mode: Mode): TableFor3[Identifier, UserAnswers, Call] =
     Table(
       ("Id", "UserAnswers", "Next Page"),
       row(IndividualPostCodeLookupId(index))(Seq(someTolerantAddress), IndividualAddressListController.onPageLoad(mode, index, None)),
@@ -49,28 +50,42 @@ class TrusteesIndividualAddressNavigatorSpec extends SpecBase with MustMatchers 
       row(TrusteePreviousAddressId(index))(someAddress, CheckYourAnswersIndividualAddressController.onPageLoad(mode, index, None))
     )
 
-    behave like navigatorWithRoutesForMode(NormalMode)(navigator, navigationForNewTrusteeIndividual(NormalMode), None)
+    behave like navigatorWithRoutesForMode(NormalMode)(navigator, normalModeRoutes(NormalMode), None)
   }
 
-//  "CheckMode" must {
-//    def checkModeRoutes(mode: Mode): TableFor3[Identifier, UserAnswers, Call] =
-//    Table(
-//      ("Id", "UserAnswers", "Expected next page"),
-//
-//    )
-//
-//    behave like navigatorWithRoutesForMode(CheckMode)(navigator, checkModeRoutes(CheckMode), None)
-//  }
+  "CheckMode" must {
+    def checkModeRoutes(mode: Mode): TableFor3[Identifier, UserAnswers, Call] =
+    Table(
+      ("Id", "UserAnswers", "Next Page"),
+      row(IndividualPostCodeLookupId(index))(Seq(someTolerantAddress), IndividualAddressListController.onPageLoad(mode, index, None)),
+      row(IndividualAddressListId(index))(someTolerantAddress, TrusteeAddressController.onPageLoad(mode, index, None)),
+      row(TrusteeAddressId(index))(someAddress, TrusteeAddressYearsController.onPageLoad(mode, index, None)),
+      row(TrusteeAddressYearsId(index))(AddressYears.UnderAYear, IndividualPreviousAddressPostcodeLookupController.onPageLoad(mode, index, None)),
+      row(TrusteeAddressYearsId(index))(AddressYears.OverAYear, CheckYourAnswersIndividualAddressController.onPageLoad(mode, index, None)),
+      row(IndividualPreviousAddressPostCodeLookupId(index))(Seq(someTolerantAddress), TrusteePreviousAddressListController.onPageLoad(mode, index, None)),
+      row(TrusteePreviousAddressListId(index))(someTolerantAddress, TrusteePreviousAddressController.onPageLoad(mode, index, None)),
+      row(TrusteePreviousAddressId(index))(someAddress, CheckYourAnswersIndividualAddressController.onPageLoad(mode, index, None))
+    )
 
-//  "UpdateMode" must {
-//    def navigationForVarianceModeTrusteeIndividual(mode: Mode): TableFor3[Identifier, UserAnswers, Call] =
-//    Table(
-//      ("Id", "UserAnswers", "Expected next page"),
-//
-//    )
-//
-//    behave like navigatorWithRoutesForMode(UpdateMode)(navigator, navigationForVarianceModeTrusteeIndividual(UpdateMode), srn)
-//  }
+    behave like navigatorWithRoutesForMode(CheckMode)(navigator, checkModeRoutes(CheckMode), None)
+  }
+
+  "UpdateMode" must {
+    def updateMadeRoutes(mode: Mode): TableFor3[Identifier, UserAnswers, Call] =
+    Table(
+      ("Id", "UserAnswers", "Expected next page"),
+      row(IndividualPostCodeLookupId(index))(Seq(someTolerantAddress), IndividualAddressListController.onPageLoad(mode, index, srn)),
+      row(IndividualAddressListId(index))(someTolerantAddress, TrusteeAddressController.onPageLoad(mode, index, srn)),
+      row(TrusteeAddressId(index))(someAddress, TrusteeAddressYearsController.onPageLoad(mode, index, srn)),
+      row(TrusteeAddressYearsId(index))(AddressYears.UnderAYear, IndividualPreviousAddressPostcodeLookupController.onPageLoad(mode, index, srn)),
+      row(TrusteeAddressYearsId(index))(AddressYears.OverAYear, CheckYourAnswersIndividualAddressController.onPageLoad(mode, index, srn)),
+      row(IndividualPreviousAddressPostCodeLookupId(index))(Seq(someTolerantAddress), TrusteePreviousAddressListController.onPageLoad(mode, index, srn)),
+      row(TrusteePreviousAddressListId(index))(someTolerantAddress, TrusteePreviousAddressController.onPageLoad(mode, index, srn)),
+      row(TrusteePreviousAddressId(index))(someAddress, AnyMoreChangesController.onPageLoad(srn))
+    )
+
+    behave like navigatorWithRoutesForMode(UpdateMode)(navigator, updateMadeRoutes(UpdateMode), srn)
+  }
 
 
 //  "CheckUpdateMode" must {
