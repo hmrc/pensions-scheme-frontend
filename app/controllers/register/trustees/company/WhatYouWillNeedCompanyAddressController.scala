a53551103114f5fa51dddee7ad2802f0a406b6cf
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2019-08-23 14:23:28 +0100

    PODS-3130 Trustee company address confirmation url.

--- app/controllers/register/trustees/company/WhatYouWillNeedCompanyAddressController.scala
+++ app/controllers/register/trustees/company/WhatYouWillNeedCompanyAddressController.scala
@@ -41,9 +41,9 @@ class WhatYouWillNeedCompanyAddressController @Inject()(appConfig: FrontendAppCo
   def onPageLoad(mode: Mode, index: Index, srn: Option[String] = None): Action[AnyContent] = (authenticate andThen
     getData(mode, srn) andThen allowAccess(srn) andThen requireData).async {
     implicit request =>
-      identifiers.register.trustees.company.CompanyDetailsId(index).retrieve.right.map {
+      CompanyDetailsId(index).retrieve.right.map {
         case CompanyDetails(companyName, _) =>
-          val pageHeader = Message("messages__whatYouWillNeedAddress__h1", companyName)
+          val pageHeader = Message("messages__addressFor", companyName)
           val href = controllers.register.trustees.company.routes.CompanyPostCodeLookupController.onSubmit(mode, index, srn)
           Future.successful(Ok(whatYouWillNeedCompanyAddress(appConfig, existingSchemeName, href, srn, pageHeader)))
       }
