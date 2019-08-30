0d9a38a60afd10b7fd9c1d17bc68a1bd5df904ee
Vincent Vinnicombe <->
2019-08-30 13:53:47 +0100

    PODS-897: Making empty forms into links, made links clickable with spacebar, added hidden description to button, wip

--- app/controllers/register/establishers/company/director/WhatYouWillNeedDirectorController.scala
+++ app/controllers/register/establishers/company/director/WhatYouWillNeedDirectorController.scala
@@ -35,23 +35,31 @@ class WhatYouWillNeedDirectorController @Inject()(appConfig: FrontendAppConfig,
                                                   getData: DataRetrievalAction,
                                                   allowAccess: AllowAccessActionProvider,
                                                   requireData: DataRequiredAction
-                                                    ) extends FrontendController with I18nSupport with Retrievals{
+                                                 ) extends FrontendController with I18nSupport with Retrievals {
 
   def onPageLoad(mode: Mode, srn: Option[String] = None, establisherIndex: Index): Action[AnyContent] = (authenticate andThen
     getData(mode, srn) andThen allowAccess(srn) andThen requireData).async {
     implicit request =>
-      val postCall = routes.WhatYouWillNeedDirectorController.onSubmit(mode, srn, establisherIndex)
+      val directorIndex = request.userAnswers.allDirectorsHnS(establisherIndex).size
       CompanyDetailsId(establisherIndex).retrieve.right.map { companyDetails =>
-        Future.successful(Ok(whatYouWillNeed(appConfig, existingSchemeName, postCall, srn,companyDetails.companyName)))
+        Future.successful(Ok(whatYouWillNeed(
+          appConfig,
+          existingSchemeName,
+          srn,
+          companyDetails.companyName,
+          mode,
+          establisherIndex,
+          directorIndex
+        )))
       }
 
   }
 
-  def onSubmit(mode: Mode, srn: Option[String] = None, establisherIndex: Index): Action[AnyContent] = (authenticate andThen
-    getData(mode, srn) andThen requireData).async {
-    implicit request =>
-      val directorIndex = request.userAnswers.allDirectorsHnS(establisherIndex).size
-      Future.successful(
-        Redirect(routes.DirectorNameController.onPageLoad(mode, establisherIndex, directorIndex, srn)))
-  }
+  //  def onSubmit(mode: Mode, srn: Option[String] = None, establisherIndex: Index): Action[AnyContent] = (authenticate andThen
+  //    getData(mode, srn) andThen requireData).async {
+  //    implicit request =>
+  //
+  //      Future.successful(
+  //        Redirect(routes.DirectorNameController.onPageLoad(mode, establisherIndex, directorIndex, srn)))
+  //  }
 }
