43e2e3ab98fb86f6eb973d92d8169c746edf1268
Ashwani1403 <ashwani.kumar@digital.hmrc.gov.uk>
2019-09-05 14:39:17 +0100

    PODS-3308-Prevent the double clicking of declaration form

--- app/assets/javascripts/pensionsschemefrontend.js
+++ app/assets/javascripts/pensionsschemefrontend.js
@@ -19,7 +19,12 @@ $(document).ready(function() {
         e.preventDefault();
         window.history.back();
     })
-
+    $("#declarationId").submit(function() {
+        $(this).submit(function() {
+          return false;
+        });
+        return true;
+    });
 
     if(document.querySelectorAll('select').length > 0){
         var graphUrl = '/register-pension-scheme/assets/javascripts/autocomplete/location-autocomplete-graph.json'
