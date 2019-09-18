f17a15ad13568cc4cf4f4b1cd59c49949eb38adf
Suryabala Singh <22298166+suryabalasingh@users.noreply.github.com>
2019-09-18 16:14:16 +0100

    PODS-3250-Added nav

--- app/navigators/establishers/partnership/EstablisherPartnershipDetailsNavigator.scala
+++ app/navigators/establishers/partnership/EstablisherPartnershipDetailsNavigator.scala
@@ -19,7 +19,8 @@ package navigators.establishers.partnership
 import com.google.inject.Inject
 import connectors.UserAnswersCacheConnector
 import identifiers.Identifier
-import identifiers.register.establishers.partnership.PartnershipDetailsId
+import identifiers.register.establishers.IsEstablisherNewId
+import identifiers.register.establishers.partnership._
 import models._
 import navigators.AbstractNavigator
 import play.api.mvc.Call
@@ -29,27 +30,52 @@ class EstablisherPartnershipDetailsNavigator @Inject()(val dataCacheConnector: U
 
   import EstablisherPartnershipDetailsNavigator._
 
-  private def normalModeRoutes(mode: SubscriptionMode, ua: UserAnswers, srn: Option[String]): PartialFunction[Identifier, Call] = {
+  private def normalAndUpdateModeRoutes(mode: Mode, ua: UserAnswers, srn: Option[String]): PartialFunction[Identifier, Call] = {
     case PartnershipDetailsId(_) => addEstablisherPage(mode, srn)
+    case PartnershipUniqueTaxReferenceID(index) => vatPage(mode, index, srn)
+    case PartnershipVatId(index) => payePage(mode, index, srn)
+    case PartnershipPayeId(index) => cyaPage(mode, index, srn)
   }
 
-  private def updateModeRoutes(mode: VarianceMode, ua: UserAnswers, srn: Option[String]): PartialFunction[Identifier, Call] = {
-    case PartnershipDetailsId(_) => addEstablisherPage(mode, srn)
+  private def checkModeRoutes(mode: SubscriptionMode, ua: UserAnswers, srn: Option[String]): PartialFunction[Identifier, Call] = {
+    case PartnershipUniqueTaxReferenceID(index) => cyaPage(mode, index, srn)
+    case PartnershipVatId(index) => cyaPage(mode, index, srn)
+    case PartnershipPayeId(index) => cyaPage(mode, index, srn)
+  }
+
+  private def checkUpdateModeRoutes(mode: VarianceMode, ua: UserAnswers, srn: Option[String]): PartialFunction[Identifier, Call] = {
+    case PartnershipUniqueTaxReferenceID(index) if ua.get(IsEstablisherNewId(index)).getOrElse(false) => cyaPage(mode, index, srn)
+    case PartnershipUniqueTaxReferenceID(_) => anyMoreChangesPage(srn)
+    case PartnershipVatId(index) => cyaPage(mode, index, srn)
+    case PartnershipEnterVATId(index) => anyMoreChangesPage(srn)
+    case PartnershipPayeId(index) => cyaPage(mode, index, srn)
+    case PartnershipPayeVariationsId(index) => anyMoreChangesPage(srn)
   }
 
   override protected def routeMap(from: NavigateFrom): Option[NavigateTo] =
-    navigateTo(normalModeRoutes(NormalMode, from.userAnswers, None), from.id)
+    navigateTo(normalAndUpdateModeRoutes(NormalMode, from.userAnswers, None), from.id)
 
-  override protected def editRouteMap(from: NavigateFrom): Option[NavigateTo] = None
+  override protected def editRouteMap(from: NavigateFrom): Option[NavigateTo] =
+    navigateTo(checkModeRoutes(CheckMode, from.userAnswers, None), from.id)
 
   override protected def updateRouteMap(from: NavigateFrom, srn: Option[String]): Option[NavigateTo] =
-    navigateTo(updateModeRoutes(UpdateMode, from.userAnswers, srn), from.id)
+    navigateTo(normalAndUpdateModeRoutes(UpdateMode, from.userAnswers, srn), from.id)
 
-  override protected def checkUpdateRouteMap(from: NavigateFrom, srn: Option[String]): Option[NavigateTo] = None
+  override protected def checkUpdateRouteMap(from: NavigateFrom, srn: Option[String]): Option[NavigateTo] =
+    navigateTo(checkUpdateModeRoutes(CheckUpdateMode, from.userAnswers, srn), from.id)
 }
 
 object EstablisherPartnershipDetailsNavigator {
   private def addEstablisherPage(mode: Mode, srn: Option[String]): Call =
     controllers.register.establishers.routes.AddEstablisherController.onPageLoad(mode, srn)
+
+  private def vatPage(mode: Mode, index: Index, srn: Option[String]): Call =
+    controllers.register.establishers.partnership.routes.PartnershipVatController.onPageLoad(mode, index, srn)
+
+  private def payePage(mode: Mode, index: Index, srn: Option[String]): Call =
+    controllers.register.establishers.partnership.routes.PartnershipPayeController.onPageLoad(mode, index, srn)
+
+  private def cyaPage(mode: Mode, index: Index, srn: Option[String]): Call =
+    controllers.register.establishers.partnership.routes.CheckYourAnswersPartnershipDetailsController.onPageLoad(mode, index, srn)
 }
 
--- test/navigators/establishers/partnership/EstablisherPartnershipDetailsNavigatorSpec.scala
+++ test/navigators/establishers/partnership/EstablisherPartnershipDetailsNavigatorSpec.scala
@@ -19,10 +19,12 @@ package navigators.establishers.partnership
 import base.SpecBase
 import generators.Generators
 import identifiers.Identifier
-import identifiers.register.establishers.partnership.PartnershipDetailsId
+import identifiers.register.establishers.IsEstablisherNewId
+import identifiers.register.establishers.partnership._
 import models._
+import navigators.establishers.individual.EstablishersIndividualDetailsNavigatorSpec.index
 import navigators.{Navigator, NavigatorBehaviour}
-import org.scalatest.MustMatchers
+import org.scalatest.{MustMatchers, OptionValues}
 import org.scalatest.prop._
 import play.api.mvc.Call
 import utils.UserAnswers
@@ -35,35 +37,79 @@ class EstablisherPartnershipDetailsNavigatorSpec extends SpecBase with MustMatch
     applicationBuilder(dataRetrievalAction = UserAnswers().dataRetrievalAction, featureSwitchEnabled = true).build().injector.instanceOf[Navigator]
 
   "EstablisherPartnershipDetailsNavigator" when {
+
     "in NormalMode" must {
-      def normalModeRoutes: TableFor3[Identifier, UserAnswers, Call] =
+      def normalModeRoutes(): TableFor3[Identifier, UserAnswers, Call] =
         Table(
           ("Id", "UserAnswers", "Next Page"),
-          row(PartnershipDetailsId(index))(partnershipDetails, addEstablisherPage(NormalMode, None))
+          row(PartnershipDetailsId(index))(partnershipDetails, addEstablisherPage(NormalMode, None)),
+          row(PartnershipUniqueTaxReferenceID(index))(UniqueTaxReference.Yes(someStringValue), vatPage(NormalMode, None)),
+          row(PartnershipVatId(index))(Vat.Yes(someStringValue), payePage(NormalMode, None)),
+          row(PartnershipPayeId(index))(Paye.Yes(someStringValue), cyaPage(NormalMode, None))
         )
 
-      behave like navigatorWithRoutesForMode(NormalMode)(navigator, normalModeRoutes, None)
+      behave like navigatorWithRoutesForMode(NormalMode)(navigator, normalModeRoutes(), None)
+    }
+
+    "in CheckMode" must {
+      def checkModeRoutes(): TableFor3[Identifier, UserAnswers, Call] =
+        Table(
+          ("Id", "UserAnswers", "Next Page"),
+          row(PartnershipUniqueTaxReferenceID(index))(UniqueTaxReference.Yes(someStringValue), cyaPage(CheckMode, None)),
+          row(PartnershipVatId(index))(Vat.Yes(someStringValue), cyaPage(CheckMode, None)),
+          row(PartnershipPayeId(index))(Paye.Yes(someStringValue), cyaPage(CheckMode, None))
+        )
+
+      behave like navigatorWithRoutesForMode(CheckMode)(navigator, checkModeRoutes(), None)
     }
 
     "in UpdateMode" must {
       def updateModeRoutes(): TableFor3[Identifier, UserAnswers, Call] =
         Table(
           ("Id", "UserAnswers", "Next Page"),
-          row(PartnershipDetailsId(index))(partnershipDetails, addEstablisherPage(UpdateMode, srn))
+          row(PartnershipDetailsId(index))(partnershipDetails, addEstablisherPage(UpdateMode, srn)),
+          row(PartnershipUniqueTaxReferenceID(index))(UniqueTaxReference.Yes(someStringValue), vatPage(UpdateMode, srn)),
+          row(PartnershipVatId(index))(Vat.Yes(someStringValue), payePage(UpdateMode, srn)),
+          row(PartnershipPayeId(index))(Paye.Yes(someStringValue), cyaPage(UpdateMode, srn))
         )
 
       behave like navigatorWithRoutesForMode(UpdateMode)(navigator, updateModeRoutes(), srn)
     }
+
+    "in CheckUpdateMode" must {
+      def checkUpdateModeRoutes(): TableFor3[Identifier, UserAnswers, Call] =
+        Table(
+          ("Id", "UserAnswers", "Next Page"),
+          row(PartnershipUniqueTaxReferenceID(index))(UniqueTaxReference.Yes(someStringValue), cyaPage(CheckUpdateMode, srn), Some(uaNewEstablisher)),
+          row(PartnershipUniqueTaxReferenceID(index))(UniqueTaxReference.Yes(someStringValue), anyMoreChangesPage(srn)),
+          row(PartnershipVatId(index))(Vat.Yes(someStringValue), cyaPage(CheckUpdateMode, srn)),
+          row(PartnershipEnterVATId(index))(someRefValue, anyMoreChangesPage(srn)),
+          row(PartnershipPayeId(index))(Paye.Yes(someStringValue), cyaPage(CheckUpdateMode, srn)),
+          row(PartnershipPayeVariationsId(index))(someRefValue, anyMoreChangesPage(srn))
+        )
+
+      behave like navigatorWithRoutesForMode(CheckUpdateMode)(navigator, checkUpdateModeRoutes(), srn)
+    }
   }
 }
 
-object EstablisherPartnershipDetailsNavigatorSpec {
+object EstablisherPartnershipDetailsNavigatorSpec extends OptionValues {
   private val index = 0
   private val srn = Some("test-srn")
   private val partnershipDetails = PartnershipDetails("test partnership")
+  private val uaNewEstablisher = UserAnswers().set(IsEstablisherNewId(index))(true).asOpt.value
 
   private def addEstablisherPage(mode: Mode, srn: Option[String]): Call =
     controllers.register.establishers.routes.AddEstablisherController.onPageLoad(mode, srn)
+
+  private def vatPage(mode: Mode, srn: Option[String]): Call =
+    controllers.register.establishers.partnership.routes.PartnershipVatController.onPageLoad(mode, index, srn)
+
+  private def payePage(mode: Mode, srn: Option[String]): Call =
+    controllers.register.establishers.partnership.routes.PartnershipPayeController.onPageLoad(mode, index, srn)
+
+  private def cyaPage(mode: Mode, srn: Option[String]): Call =
+    controllers.register.establishers.partnership.routes.CheckYourAnswersPartnershipDetailsController.onPageLoad(mode, index, srn)
 }
 
 
