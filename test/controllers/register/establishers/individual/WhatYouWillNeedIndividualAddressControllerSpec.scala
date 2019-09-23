f4f604b8559723ef5c00a5d02dda33d555c83d0a
Vincent Vinnicombe <->
2019-09-23 11:53:47 +0100

    PODS-3230: Updated spec

--- test/controllers/register/establishers/individual/WhatYouWillNeedIndividualAddressControllerSpec.scala
+++ test/controllers/register/establishers/individual/WhatYouWillNeedIndividualAddressControllerSpec.scala
@@ -39,7 +39,7 @@ class WhatYouWillNeedIndividualAddressControllerSpec extends ControllerSpecBase 
 
   private def viewAsString(): String =
     whatYouWillNeedIndividualAddress(
-      frontendAppConfig, None, href, None, "Test Name", Message("messages__theEstablisher")
+      frontendAppConfig, None, href, None, "Test Name", Message("messages__thePerson")
     )(fakeRequest, messages).toString
 
   "WhatYouWillNeedIndividualAddressController" when {
