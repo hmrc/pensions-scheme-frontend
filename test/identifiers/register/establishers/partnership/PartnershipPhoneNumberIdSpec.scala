de263236c203833e1db59842088939750cef4b2f
Madhura <madhuranakate@gmail.com>
2019-09-26 15:09:30 +0100

    PODS-3273 Est Partnership Contatc CYA page

---
+++ test/identifiers/register/establishers/partnership/PartnershipPhoneNumberIdSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package identifiers.register.establishers.partnership
+
+import base.SpecBase
+import models.requests.DataRequest
+import models.{Link, NormalMode, PartnershipDetails, UpdateMode}
+import play.api.mvc.AnyContent
+import play.api.test.FakeRequest
+import uk.gov.hmrc.domain.PsaId
+import utils.checkyouranswers.Ops._
+import utils.{CountryOptions, InputOption, UserAnswers}
+import viewmodels.{AnswerRow, Message}
+
+class PartnershipPhoneNumberIdSpec extends SpecBase {
+
+  "cya" when {
+    implicit val countryOptions: CountryOptions = new CountryOptions(Seq.empty[InputOption])
+    val index = 0
+    val phone = "0111"
+    val onwardUrl = "onwardUrl"
+    val partnershipDetails = PartnershipDetails("test partnership")
+    Seq(NormalMode, UpdateMode).foreach { mode =>
+
+      s"in ${mode.toString} mode" must {
+        "return answers rows with change links" in {
+          val request: DataRequest[AnyContent] = DataRequest(FakeRequest(), "id",
+            UserAnswers().establisherPartnershipDetails(index, partnershipDetails)
+              .set(PartnershipPhoneNumberId(index))(phone).asOpt.value, PsaId("A0000000"))
+          implicit val userAnswers: UserAnswers = request.userAnswers
+
+          PartnershipPhoneNumberId(0).row(onwardUrl, mode)(request, implicitly) must equal(Seq(
+            AnswerRow(
+              Message("messages__common_phone__heading", partnershipDetails.name),
+              Seq(phone),
+              answerIsMessageKey = false,
+              Some(Link("site.change", onwardUrl, Some(Message("messages__visuallyhidden__dynamic_phone", partnershipDetails.name))))
+            )))
+        }
+      }
+    }
+  }
+}
