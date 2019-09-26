0881ed0bcb9731cec8ea3bd5a9154c4bdbb7d3f6
Madhura <madhuranakate@gmail.com>
2019-09-26 14:46:00 +0100

    PODS-3389 Establisher partnership phone number

---
+++ test/controllers/register/establishers/partnership/PartnershipPhoneNumberControllerSpec.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.establishers.partnership
+
+import controllers.ControllerSpecBase
+import forms.PhoneFormProvider
+import models.{Index, NormalMode, PartnershipDetails}
+import navigators.Navigator
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.mockito.MockitoSugar
+import play.api.data.Form
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.mvc.Call
+import play.api.test.Helpers._
+import services.{FakeUserAnswersService, UserAnswersService}
+import utils.{FakeNavigator, UserAnswers}
+import viewmodels.{CommonFormWithHintViewModel, Message}
+import views.html.phoneNumber
+
+class PartnershipPhoneNumberControllerSpec extends ControllerSpecBase with MockitoSugar with BeforeAndAfterEach {
+
+  def onwardRoute: Call = controllers.routes.IndexController.onPageLoad()
+
+  private val formProvider = new PhoneFormProvider()
+  private val form: Form[String] = formProvider()
+  private val firstIndex = Index(0)
+  private val phone = "1234"
+  private val establisherPartnershipDetails = PartnershipDetails("test partnership")
+
+  private val fullAnswers = UserAnswers().establisherPartnershipDetails(firstIndex, establisherPartnershipDetails)
+
+  def viewAsString(form: Form[_] = form): String =
+    phoneNumber(
+      frontendAppConfig,
+      form,
+      CommonFormWithHintViewModel(
+        routes.PartnershipPhoneNumberController.onSubmit(NormalMode, firstIndex, None),
+        Message("messages__partnership_phone__title"),
+        Message("messages__common_phone__heading", establisherPartnershipDetails.name),
+        Some(Message("messages__phone__hint")),
+        None
+      ),
+      None
+    )(fakeRequest, messages).toString
+
+  "PartnershipPhoneNumberController" when {
+
+    "on a GET" must {
+      "return OK and the correct view" in {
+        running(_.overrides(modules(fullAnswers.dataRetrievalAction, featureSwitchEnabled = true): _*)) {
+          app =>
+            val controller = app.injector.instanceOf[PartnershipPhoneNumberController]
+            val result = controller.onPageLoad(NormalMode, firstIndex, None)(fakeRequest)
+
+            status(result) mustBe OK
+            contentAsString(result) mustBe viewAsString()
+        }
+      }
+    }
+
+    "on a POST" must {
+      "redirect to relevant page" in {
+        running(_.overrides(
+          modules(fullAnswers.dataRetrievalAction, featureSwitchEnabled = true) ++
+            Seq[GuiceableModule](bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+              bind[UserAnswersService].toInstance(FakeUserAnswersService)
+            ): _*)) {
+          app =>
+            val controller = app.injector.instanceOf[PartnershipPhoneNumberController]
+            val postRequest = fakeRequest.withFormUrlEncodedBody(("phone", phone))
+            val result = controller.onSubmit(NormalMode, firstIndex, None)(postRequest)
+
+            status(result) mustBe SEE_OTHER
+            redirectLocation(result) mustBe Some(onwardRoute.url)
+        }
+      }
+    }
+  }
+}
