2ac321f578fe2cd078e467599c3c0186ce4e8f91
Madhura <madhuranakate@gmail.com>
2019-09-30 08:35:36 +0100

    PODS-3272 Est partnership contact spoke WYN page (#952)

---
+++ test/controllers/register/establishers/partnership/WhatYouWillNeedPartnershipContactDetailsControllerSpec.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.establishers.partnership
+
+import controllers.ControllerSpecBase
+import controllers.actions._
+import models.{Index, NormalMode, PartnershipDetails}
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.Helpers._
+import utils.UserAnswers
+import viewmodels.{CommonFormWithHintViewModel, Message}
+import views.html.register.whatYouWillNeedPartnershipContactDetails
+
+class WhatYouWillNeedPartnershipContactDetailsControllerSpec extends ControllerSpecBase {
+  private val index = 0
+  private val establisherPartnership = PartnershipDetails("partnership Name")
+
+  def onwardRoute: Call = controllers.register.establishers.company.routes.CompanyEmailController.onPageLoad(NormalMode, None, Index(0))
+
+  def viewAsString(): String = whatYouWillNeedPartnershipContactDetails(
+    frontendAppConfig,
+    None,
+    CommonFormWithHintViewModel(
+      postCall = controllers.register.establishers.partnership.routes.PartnershipEmailController.onPageLoad(NormalMode, index, None),
+      title = Message("messages__whatYouWillNeedPartnershipContact__title"),
+      heading = Message("messages__whatYouWillNeedPartnershipContact__h1", establisherPartnership.name),
+      srn = None
+    ))(fakeRequest, messages).toString
+
+  "WhatYouWillNeedPartnershipContactDetailsController" when {
+
+    "on a GET" must {
+      "return OK and the correct view" in {
+        running(_.overrides(
+          bind[AuthAction].toInstance(FakeAuthAction),
+          bind[AllowAccessActionProvider].toInstance(FakeAllowAccessProvider()),
+          bind[DataRetrievalAction].toInstance(UserAnswers().establisherPartnershipDetails(index, establisherPartnership).dataRetrievalAction)
+        )) { app =>
+          val controller = app.injector.instanceOf[WhatYouWillNeedPartnershipContactDetailsController]
+          val result = controller.onPageLoad(NormalMode, index, None)(fakeRequest)
+
+          status(result) mustBe OK
+          contentAsString(result) mustBe viewAsString()
+        }
+      }
+    }
+  }
+}
+
