48ca6e6161a8a73539dc045b05d13084b9434722
Suryabala Singh <22298166+suryabalasingh@users.noreply.github.com>
2019-10-01 13:26:21 +0100

    PODS-3361-Fixed test

--- test/controllers/register/establishers/individual/EstablisherEmailControllerSpec.scala
+++ test/controllers/register/establishers/individual/EstablisherEmailControllerSpec.scala
@@ -50,7 +50,7 @@ class EstablisherEmailControllerSpec extends ControllerSpecBase {
       form,
       CommonFormWithHintViewModel(
         EstablisherEmailController.onSubmit(NormalMode, firstIndex, None),
-        Message("messages__enterEmail", Message("messages__theCompany").resolve),
+        Message("messages__enterEmail", Message("messages__theIndividual").resolve),
         Message("messages__enterEmail", establisherName.fullName),
         Some(Message("messages__email_dynamic__hint", establisherName.fullName)),
         None
