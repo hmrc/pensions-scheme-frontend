4faa2c7470bfb7ddc388b332b67d5f4b1cc05031
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-10-04 09:39:53 +0100

    PODS-3217 Unit tests

--- test/controllers/register/establishers/partnership/partner/PartnerPreviousAddressControllerSpec.scala
+++ test/controllers/register/establishers/partnership/partner/PartnerPreviousAddressControllerSpec.scala
@@ -87,7 +87,7 @@ class PartnerPreviousAddressControllerSpec extends ControllerSpecBase with Mocki
           val viewmodel = ManualAddressViewModel(
             controller.postCall(NormalMode, establisherIndex, partnerIndex, None),
             countryOptions.options,
-            controller.title,
+            Message(controller.heading,Message("messages__thePartner").resolve),
             Message(controller.heading,partnerDetails.fullName)
           )
 
