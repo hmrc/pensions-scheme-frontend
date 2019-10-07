b78b1b3cc07fdeba9f24df2dddf6ae4f778165e9
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-10-07 14:49:09 +0100

    PODS-3385 Remove clean up and tests

--- app/identifiers/register/establishers/EstablisherKindId.scala
+++ app/identifiers/register/establishers/EstablisherKindId.scala
@@ -17,101 +17,11 @@
 package identifiers.register.establishers
 
 import identifiers.TypedIdentifier
-import identifiers.register.establishers.company._
-import identifiers.register.establishers.company.director.{DirectorId, DirectorNameId}
-import identifiers.register.establishers.individual._
-import identifiers.register.establishers.partnership._
-import identifiers.register.establishers.partnership.partner.{PartnerDetailsId, PartnerId}
-import models.person.{PersonDetails, PersonName}
 import models.register.establishers.EstablisherKind
-import play.api.libs.json.{JsPath, JsResult, JsSuccess}
-import utils.UserAnswers
+import play.api.libs.json.JsPath
 
 case class EstablisherKindId(index: Int) extends TypedIdentifier[EstablisherKind] {
   override def path: JsPath = EstablishersId(index).path \ EstablisherKindId.toString
-
-  override def cleanup(value: Option[EstablisherKind], userAnswers: UserAnswers): JsResult[UserAnswers] =
-    value match {
-      case Some(EstablisherKind.Indivdual) =>
-        removeAllCompany(userAnswers)
-          .flatMap(removeAllPartnership(_))
-
-      case Some(EstablisherKind.Company) =>
-        removeAllIndividual(userAnswers)
-          .flatMap(removeAllPartnership(_))
-
-      case Some(EstablisherKind.Partnership) =>
-        removeAllCompany(userAnswers)
-          .flatMap(removeAllIndividual(_))
-
-      case _ =>
-        super.cleanup(value, userAnswers)
-    }
-
-  private def removeAllDirectors(userAnswers: UserAnswers): JsResult[UserAnswers] = {
-    userAnswers.getAllRecursive[PersonName](DirectorNameId.collectionPath(index)) match {
-      case Some(allDirectors) if allDirectors.nonEmpty =>
-        userAnswers.remove(DirectorId(index, 0)).flatMap(removeAllDirectors)
-      case _ =>
-        JsSuccess(userAnswers)
-    }
-  }
-
-  private def removeAllCompany(userAnswers: UserAnswers): JsResult[UserAnswers] = {
-    userAnswers.remove(CompanyDetailsId(index))
-
-      .flatMap(_.remove(HasCompanyCRNId(index)))
-      .flatMap(_.remove(CompanyEnterCRNId(index)))
-      .flatMap(_.remove(HasCompanyUTRId(index)))
-      .flatMap(_.remove(CompanyEnterUTRId(index)))
-
-      .flatMap(_.remove(CompanyPostCodeLookupId(index)))
-      .flatMap(_.remove(CompanyAddressListId(index)))
-      .flatMap(_.remove(CompanyAddressId(index)))
-      .flatMap(_.remove(CompanyAddressYearsId(index)))
-      .flatMap(_.remove(CompanyPreviousAddressPostcodeLookupId(index)))
-      .flatMap(_.remove(CompanyPreviousAddressListId(index)))
-      .flatMap(_.remove(CompanyPreviousAddressId(index)))
-      .flatMap(removeAllDirectors)
-  }
-
-  private def removeAllPartners(userAnswers: UserAnswers): JsResult[UserAnswers] = {
-    userAnswers.getAllRecursive[PersonDetails](PartnerDetailsId.collectionPath(index)) match {
-      case Some(allPartners) if allPartners.nonEmpty =>
-        userAnswers.remove(PartnerId(index, 0)).flatMap(removeAllPartners)
-      case _ =>
-        JsSuccess(userAnswers)
-    }
-  }
-
-  private def removeAllPartnership(userAnswers: UserAnswers): JsResult[UserAnswers] = {
-    userAnswers.remove(PartnershipDetailsId(index))
-      .flatMap(_.remove(PartnershipVatId(index)))
-      .flatMap(_.remove(PartnershipPayeId(index)))
-      .flatMap(_.remove(PartnershipUniqueTaxReferenceID(index)))
-      .flatMap(_.remove(PartnershipPostcodeLookupId(index)))
-      .flatMap(_.remove(PartnershipAddressListId(index)))
-      .flatMap(_.remove(PartnershipAddressId(index)))
-      .flatMap(_.remove(PartnershipAddressYearsId(index)))
-      .flatMap(_.remove(PartnershipPreviousAddressPostcodeLookupId(index)))
-      .flatMap(_.remove(PartnershipPreviousAddressListId(index)))
-      .flatMap(_.remove(PartnershipPreviousAddressId(index)))
-      .flatMap(_.remove(PartnershipContactDetailsId(index)))
-      .flatMap(removeAllPartners)
-  }
-
-  private def removeAllIndividual(userAnswers: UserAnswers): JsResult[UserAnswers] = {
-    userAnswers.remove(EstablisherDetailsId(index))
-      .flatMap(_.remove(EstablisherNinoId(index)))
-      .flatMap(_.remove(UniqueTaxReferenceId(index)))
-      .flatMap(_.remove(PostCodeLookupId(index)))
-      .flatMap(_.remove(AddressListId(index)))
-      .flatMap(_.remove(AddressId(index)))
-      .flatMap(_.remove(AddressYearsId(index)))
-      .flatMap(_.remove(PreviousPostCodeLookupId(index)))
-      .flatMap(_.remove(PreviousAddressId(index)))
-      .flatMap(_.remove(ContactDetailsId(index)))
-  }
 }
 
 object EstablisherKindId {
--- app/identifiers/register/trustees/TrusteeKindId.scala
+++ app/identifiers/register/trustees/TrusteeKindId.scala
@@ -17,66 +17,14 @@
 package identifiers.register.trustees
 
 import identifiers._
-import identifiers.register.trustees.company._
-import identifiers.register.trustees.individual._
-import identifiers.register.trustees.partnership._
-import models.Index
 import models.register.trustees.TrusteeKind
-import play.api.libs.json.{JsPath, JsResult, __}
-import utils.UserAnswers
+import play.api.libs.json.{JsPath, __}
 
 case class TrusteeKindId(index: Int) extends TypedIdentifier[TrusteeKind] {
-
-  import TrusteeKindId._
-
   override def path: JsPath = TrusteesId(index).path \ TrusteeKindId.toString
-
-  override def cleanup(value: Option[TrusteeKind], userAnswers: UserAnswers): JsResult[UserAnswers] = {
-    value match {
-      case Some(TrusteeKind.Individual) =>
-        userAnswers.removeAllOf(companyIdList(index) ++ partnershipIdList(index))
-      case Some(TrusteeKind.Company) =>
-        userAnswers.removeAllOf(individualIdList(index) ++ partnershipIdList(index))
-      case Some(TrusteeKind.Partnership) =>
-        userAnswers.removeAllOf(companyIdList(index) ++ individualIdList(index))
-      case _ => super.cleanup(value, userAnswers)
-    }
-  }
 }
 
 object TrusteeKindId {
-  private def partnershipIdList(index: Index): List[TypedIdentifier[_]] = List(
-    PartnershipDetailsId(index),
-    PartnershipPostcodeLookupId(index), PartnershipAddressListId(index), PartnershipAddressId(index), PartnershipAddressYearsId(index),
-    PartnershipPreviousAddressPostcodeLookupId(index), PartnershipPreviousAddressId(index), PartnershipPreviousAddressListId(index),
-    PartnershipContactDetailsId(index)
-  )
-
-  private def companyIdList(index: Index): List[TypedIdentifier[_]] = List(
-    CompanyDetailsId(index),
-    HasCompanyCRNId(index),
-    CompanyEnterCRNId(index),
-    HasCompanyUTRId(index),
-    CompanyEnterUTRId(index),
-    CompanyPostcodeLookupId(index),
-    CompanyAddressId(index),
-    CompanyAddressYearsId(index),
-    CompanyPreviousAddressPostcodeLookupId(index),
-    CompanyPreviousAddressId(index)
-//    CompanyEmailId(index)
-//    company.CompanyPhoneId(index)
-  )
-
-  private def individualIdList(index: Index): List[TypedIdentifier[_]] = List(
-    TrusteeNameId(index),
-    TrusteeEnterNINOId(index),
-    IndividualPostCodeLookupId(index),
-    TrusteeAddressId(index),
-    TrusteeAddressYearsId(index),
-    IndividualPreviousAddressPostCodeLookupId(index),
-    TrusteePreviousAddressId(index)
-  )
-
   def collectionPath: JsPath = __ \ TrusteesId.toString \\ TrusteeKindId.toString
 
   override def toString: String = "trusteeKind"
