9ca2f0a0048da9df4bf148104c7bbe5986af3f2b
Vincent Vinnicombe <->
2019-11-01 08:54:59 +0000

    PODS-2790: Inlined library version vals, removed hmrctest as not used

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -11,40 +11,20 @@ private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport._
 
-  private val playHealthVersion = "3.14.0-play-25"
-  private val logbackJsonLoggerVersion = "3.1.0"
-  private val govukTemplateVersion = "5.26.0-play-25"
-  private val hmrcTestVersion = "3.9.0-play-25"
-  private val scalaTestVersion = "3.0.4"
-  private val scalaTestPlusPlayVersion = "2.0.1"
-  private val playUiVersion = "8.3.0-play-25"
-  private val pegdownVersion = "1.6.0"
-  private val mockitoAllVersion = "1.10.19"
-  private val httpCachingClientVersion = "9.0.0-play-25"
-  private val playReactivemongoVersion = "6.7.0"
-  private val playConditionalFormMappingVersion = "0.2.0"
-  private val playLanguageVersion = "3.4.0"
-  private val bootstrapVersion = "5.1.0"
-  private val domainVersion = "5.6.0-play-25"
-  private val scalacheckGenRegexp = "0.1.1"
-  private val wireMockVersion = "2.18.0"
-  private val whitelistVersion = "2.0.0"
-  private val guiceVersion = "4.2.2"
-
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion,
-    "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
-    "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
-    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
-    "uk.gov.hmrc" %% "play-ui" % playUiVersion,
-    "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
-    "uk.gov.hmrc" %% "play-conditional-form-mapping" % playConditionalFormMappingVersion,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-language" % playLanguageVersion,
-    "uk.gov.hmrc" %% "domain" % domainVersion,
-    "uk.gov.hmrc" %% "play-whitelist-filter" % whitelistVersion,
-    "com.google.inject.extensions" % "guice-multibindings" % guiceVersion
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.7.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.26.0-play-25",
+    "uk.gov.hmrc" %% "play-health" % "3.14.0-play-25",
+    "uk.gov.hmrc" %% "play-ui" % "8.3.0-play-25",
+    "uk.gov.hmrc" %% "http-caching-client" % "9.0.0-play-25",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "0.2.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "5.1.0",
+    "uk.gov.hmrc" %% "play-language" % "3.4.0",
+    "uk.gov.hmrc" %% "domain" % "5.6.0-play-25",
+    "uk.gov.hmrc" %% "play-whitelist-filter" % "2.0.0",
+    "com.google.inject.extensions" % "guice-multibindings" % "4.2.2"
   )
 
   trait TestDependencies {
@@ -55,17 +35,15 @@ private object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test: Seq[ModuleID] = Seq(
-        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
-        "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
-        "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % scope,
+        "org.scalatest" %% "scalatest" % "3.0.4" % scope,
+        "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
         "org.scalacheck" %% "scalacheck" % "1.14.0" % scope,
-        "org.pegdown" % "pegdown" % pegdownVersion % scope,
+        "org.pegdown" % "pegdown" % "1.6.0" % scope,
         "org.jsoup" % "jsoup" % "1.11.3" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "org.mockito" % "mockito-all" % mockitoAllVersion % scope,
-        "com.github.tomakehurst" % "wiremock" % wireMockVersion % scope,
-        "wolfendale" %% "scalacheck-gen-regexp" % scalacheckGenRegexp % scope
-
+        "org.mockito" % "mockito-all" % "1.10.19" % scope,
+        "com.github.tomakehurst" % "wiremock" % "2.18.0" % scope,
+        "wolfendale" %% "scalacheck-gen-regexp" % "0.1.1" % scope
       )
     }.test
   }
