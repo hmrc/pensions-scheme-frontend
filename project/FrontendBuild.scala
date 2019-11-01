c94e7ac35745e772668b74278b0f63674212bd89
Vincent Vinnicombe <->
2019-11-01 08:47:52 +0000

    PODS-2790: Updated libraries, fixed deprecation warnings

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -14,24 +14,24 @@ private object AppDependencies {
   private val playHealthVersion = "3.14.0-play-25"
   private val logbackJsonLoggerVersion = "3.1.0"
   private val govukTemplateVersion = "5.26.0-play-25"
-  private val hmrcTestVersion = "3.8.0-play-25"
+  private val hmrcTestVersion = "3.9.0-play-25"
   private val scalaTestVersion = "3.0.4"
   private val scalaTestPlusPlayVersion = "2.0.1"
-  private val playUiVersion = "7.40.0-play-25"
+  private val playUiVersion = "8.3.0-play-25"
   private val pegdownVersion = "1.6.0"
   private val mockitoAllVersion = "1.10.19"
-  private val httpCachingClientVersion = "8.4.0-play-25"
+  private val httpCachingClientVersion = "9.0.0-play-25"
   private val playReactivemongoVersion = "6.7.0"
   private val playConditionalFormMappingVersion = "0.2.0"
   private val playLanguageVersion = "3.4.0"
-  private val bootstrapVersion = "4.12.0"
+  private val bootstrapVersion = "5.1.0"
   private val domainVersion = "5.6.0-play-25"
   private val scalacheckGenRegexp = "0.1.1"
   private val wireMockVersion = "2.18.0"
   private val whitelistVersion = "2.0.0"
   private val guiceVersion = "4.2.2"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion,
     "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
@@ -53,8 +53,8 @@ private object AppDependencies {
   }
 
   object Test {
-    def apply() = new TestDependencies {
-      override lazy val test = Seq(
+    def apply(): Seq[ModuleID] = new TestDependencies {
+      override lazy val test: Seq[ModuleID] = Seq(
         "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % scope,
@@ -70,5 +70,5 @@ private object AppDependencies {
     }.test
   }
 
-  def apply() = compile ++ Test()
+  def apply(): Seq[ModuleID] = compile ++ Test()
 }
