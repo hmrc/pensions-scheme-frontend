4cc275d5051b8bfa581f46b8d57e1684dd06d094
Ashwani1403 <48753134+Ashwani1403@users.noreply.github.com>
2021-04-23 01:59:56 +0100

    PODS-5341: added declaration page additonal

---
+++ app/controllers/racdac/DeclarationController.scala
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.racdac
+
+import audit.{AuditService, TcmpAuditEvent}
+import config.FrontendAppConfig
+import connectors._
+import controllers.Retrievals
+import controllers.actions._
+import controllers.register.routes.DeclarationController
+import identifiers.register._
+import identifiers.register.establishers.company.{CompanyDetailsId, IsCompanyDormantId}
+import identifiers.{MoneyPurchaseBenefitsId, SchemeTypeId, TypeOfBenefitsId}
+import models.register.DeclarationDormant
+import models.register.DeclarationDormant.Yes
+import models.register.SchemeType.MasterTrust
+import models.requests.DataRequest
+import models.{NormalMode, TypeOfBenefits}
+import navigators.Navigator
+import play.api.Logger
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.domain.PsaId
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.annotations.Register
+import utils.hstasklisthelper.HsTaskListHelperRegistration
+import utils.{Enumerable, UserAnswers}
+import views.html.racdac.declaration
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class DeclarationController @Inject()(
+                                       appConfig: FrontendAppConfig,
+                                       override val messagesApi: MessagesApi,
+                                       dataCacheConnector: UserAnswersCacheConnector,
+                                       @Register navigator: Navigator,
+                                       authenticate: AuthAction,
+                                       getData: DataRetrievalAction,
+                                       requireData: DataRequiredAction,
+                                       pensionsSchemeConnector: PensionsSchemeConnector,
+                                       pensionAdministratorConnector: PensionAdministratorConnector,
+                                       emailConnector: EmailConnector,
+                                       minimalPsaConnector: MinimalPsaConnector,
+                                       val controllerComponents: MessagesControllerComponents,
+                                       hsTaskListHelperRegistration: HsTaskListHelperRegistration,
+                                       val view: declaration,
+                                       auditService: AuditService
+                                     )(implicit val executionContext: ExecutionContext)
+  extends FrontendBaseController
+    with Retrievals
+    with I18nSupport
+    with Enumerable.Implicits {
+
+  private val logger = Logger(classOf[DeclarationController])
+
+  def onPageLoad: Action[AnyContent] = (authenticate() andThen getData() andThen requireData).async {
+    implicit request =>
+      if (hsTaskListHelperRegistration.declarationEnabled(request.userAnswers)) {
+        showPage(Ok.apply)
+      } else {
+        Future.successful(Redirect(controllers.routes.PsaSchemeTaskListController.onPageLoad(NormalMode, None)))
+      }
+  }
+
+  private def showPage(status: HtmlFormat.Appendable => Result)
+                      (implicit request: DataRequest[AnyContent]): Future[Result] = {
+
+    val isEstCompany = request.userAnswers.hasCompanies(NormalMode)
+    val href = DeclarationController.onClickAgree()
+
+    val declarationDormantValue = if (isDeclarationDormant) DeclarationDormant.values.head
+    else DeclarationDormant.values(1)
+    val readyForRender = if (isEstCompany) {
+      dataCacheConnector.save(request.externalId, DeclarationDormantId, declarationDormantValue).map(_ => ())
+    } else {
+      Future.successful(())
+    }
+
+    readyForRender.flatMap { _ =>
+      request.userAnswers.get(identifiers.DeclarationDutiesId) match {
+        case Some(hasWorkingKnowledge) =>
+          pensionAdministratorConnector.getPSAName.flatMap { psaName =>
+            Future.successful(
+            status(
+              view(
+                psaName = psaName,
+                href = href)
+            )
+            )
+          }
+
+        case _ => Future.successful(Redirect(controllers.routes.SessionExpiredController.onPageLoad()))
+      }
+    }
+  }
+
+  private def isDeclarationDormant(implicit request: DataRequest[AnyContent]): Boolean =
+    request.userAnswers.allEstablishersAfterDelete(
+      NormalMode
+    ).exists { allEstablishers =>
+      allEstablishers.id match {
+        case CompanyDetailsId(index) =>
+          isDormant(request.userAnswers.get(IsCompanyDormantId(index)))
+        case _ =>
+          false
+      }
+    }
+
+  private def isDormant(dormant: Option[DeclarationDormant]): Boolean =
+    dormant match {
+      case Some(Yes) => true
+      case _ => false
+    }
+
+  def onClickAgree: Action[AnyContent] = (authenticate() andThen getData() andThen requireData).async {
+    implicit request =>
+      val psaId: PsaId = request.psaId.getOrElse(throw MissingPsaId)
+      (for {
+        cacheMap <- dataCacheConnector.save(request.externalId, DeclarationId, value = true)
+        eitherSubmissionResponse <- pensionsSchemeConnector.registerScheme(UserAnswers(cacheMap), psaId.id)
+      } yield eitherSubmissionResponse).flatMap {
+        case Right(submissionResponse) =>
+          for {
+            cacheMap <- dataCacheConnector.save(request.externalId, SubmissionReferenceNumberId, submissionResponse)
+            _ <- sendEmail(submissionResponse.schemeReferenceNumber, psaId)
+            _ <- auditTcmp(psaId.id, request.userAnswers)
+          } yield Redirect(navigator.nextPage(DeclarationId, NormalMode, UserAnswers(cacheMap)))
+        case Left(_) =>
+          Future.successful(Redirect(controllers.routes.SessionExpiredController.onPageLoad()))
+      }
+  }
+
+
+  private def sendEmail(srn: String, psaId: PsaId)
+                       (implicit request: DataRequest[AnyContent]): Future[EmailStatus] = {
+    logger.debug("Fetch email from API")
+
+    minimalPsaConnector.getMinimalPsaDetails(psaId.id) flatMap { minimalPsa =>
+      emailConnector.sendEmail(
+        emailAddress = minimalPsa.email,
+        templateName = appConfig.emailTemplateId,
+        params = Map("srn" -> formatSrnForEmail(srn), "psaName" -> minimalPsa.name),
+        psaId = psaId
+      )
+    } recoverWith {
+      case _: Throwable => Future.successful(EmailNotSent)
+    }
+  }
+
+  //scalastyle:off magic.number
+  private def formatSrnForEmail(srn: String): String = {
+    //noinspection ScalaStyle
+    val (start, end) = srn.splitAt(6)
+    start + ' ' + end
+  }
+
+  case object MissingPsaId extends Exception("Psa ID missing in request")
+
+  private def auditTcmp(psaId: String, ua: UserAnswers)
+                       (implicit request: DataRequest[AnyContent]): Future[Unit] =
+    Future.successful(
+      ua.get(TypeOfBenefitsId) match {
+        case Some(typeOfBenefits)
+          if !typeOfBenefits.equals(TypeOfBenefits.Defined) =>
+          auditService.sendExtendedEvent(
+            TcmpAuditEvent(
+              psaId = psaId,
+              tcmp = TcmpAuditEvent.tcmpAuditValue(
+                typeOfBenefits = typeOfBenefits,
+                moneyPurchaseBenefit = ua.get(MoneyPurchaseBenefitsId)
+              ),
+              payload = ua.json,
+              auditType = "TaxationCollectiveMoneyPurchaseSubscriptionAuditEvent"
+            )
+          )
+        case _ => ()
+      }
+    )
+
+}
---
+++ test/controllers/racdac/DeclarationControllerSpec.scala
@@ -0,0 +1,348 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register
+
+import audit.{AuditService, TcmpAuditEvent}
+import connectors.{FakeUserAnswersCacheConnector, _}
+import controllers.ControllerSpecBase
+import controllers.actions._
+import forms.register.DeclarationFormProvider
+import helpers.DataCompletionHelper
+import identifiers._
+import identifiers.register.{DeclarationDormantId, DeclarationId}
+import models._
+import models.register.{DeclarationDormant, SchemeSubmissionResponse, SchemeType}
+import org.mockito.ArgumentCaptor
+import org.mockito.Matchers.{any, eq => eqTo}
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.data.Form
+import play.api.libs.json.{JsObject, Json}
+import play.api.mvc.{Call, RequestHeader}
+import play.api.test.Helpers._
+import uk.gov.hmrc.domain.PsaId
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import utils.hstasklisthelper.HsTaskListHelperRegistration
+import utils.{FakeNavigator, UserAnswers}
+import views.html.register.declaration
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class DeclarationControllerSpec
+  extends ControllerSpecBase
+    with MockitoSugar
+    with ScalaFutures
+    with BeforeAndAfterEach {
+
+  import DeclarationControllerSpec._
+
+  override protected def beforeEach(): Unit = {
+    reset(mockHsTaskListHelperRegistration)
+    when(mockHsTaskListHelperRegistration.declarationEnabled(any())).thenReturn(true)
+  }
+
+  "Declaration Controller" must {
+
+    "redirect to task list page when user answers are not complete" in {
+      when(mockHsTaskListHelperRegistration.declarationEnabled(any())).thenReturn(false)
+      val result = controller(UserAnswers().schemeName("Test Scheme").dataRetrievalAction).onPageLoad()(fakeRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result).value mustBe controllers.routes.PsaSchemeTaskListController.onPageLoad(NormalMode, None).url
+    }
+
+    "return OK and don't save the DeclarationDormant " when {
+
+      "the establisher is an individual" in {
+        val result = controller(individualEst).onPageLoad()(fakeRequest)
+
+        status(result) mustBe OK
+        contentAsString(result) mustBe viewAsString(isCompany = false, isDormant = false)
+        FakeUserAnswersCacheConnector.verifyNot(DeclarationDormantId)
+      }
+    }
+
+    "return OK, the correct view and save the DeclarationDormant" when {
+
+      "the establisher is a dormant company" in {
+        val result = controller(dormantCompany).onPageLoad()(fakeRequest)
+
+        status(result) mustBe OK
+        contentAsString(result) mustBe viewAsString(isCompany = true, isDormant = true)
+        FakeUserAnswersCacheConnector.verify(DeclarationDormantId, DeclarationDormant.values.head)
+      }
+
+      "the establisher is non dormant company" in {
+        val result = controller(nonDormantCompany).onPageLoad()(fakeRequest)
+
+        status(result) mustBe OK
+        contentAsString(result) mustBe viewAsString(isCompany = true, isDormant = false)
+        FakeUserAnswersCacheConnector.verify(DeclarationDormantId, DeclarationDormant.values(1))
+      }
+    }
+
+    "return OK and the correct view " when {
+      "master trust and all the answers is complete" in {
+
+        val result = controller(dataWithMasterTrust).onPageLoad()(fakeRequest)
+
+        status(result) mustBe OK
+        contentAsString(result) mustBe viewAsString(isCompany = false, isDormant = false, showMasterTrustDeclaration = true)
+      }
+    }
+
+    "redirect to the next page on clicking agree and continue" in {
+      val result = controller(nonDormantCompany).onClickAgree()(fakeRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(onwardRoute.url)
+    }
+
+    "redirect to the next page on clicking agree and continue and audit TCMP" in {
+      reset(mockAuditService)
+
+      val result = controller(tcmpAuditDataUa(TypeOfBenefits.MoneyPurchase).dataRetrievalAction).onClickAgree()(fakeRequest)
+
+      val argCaptor = ArgumentCaptor.forClass(classOf[TcmpAuditEvent])
+
+      val auditEvent = TcmpAuditEvent(
+        psaId = "A0000000",
+        tcmp = "01",
+        payload = Json.obj(
+          "moneyPurchaseBenefits" -> Json.arr("opt1"),
+          "benefits" -> "opt1",
+          SchemeNameId.toString -> "schemeName",
+          "declaration" -> true
+        ) ++ tcmpAuditDataUa(TypeOfBenefits.MoneyPurchase).json.as[JsObject],
+        auditType = "TaxationCollectiveMoneyPurchaseSubscriptionAuditEvent"
+      )
+
+      whenReady(result) {
+        response =>
+          response.header.status mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(onwardRoute.url)
+          verify(mockAuditService, times(1)).sendExtendedEvent(argCaptor.capture())(any(), any())
+          argCaptor.getValue mustBe auditEvent
+      }
+    }
+
+    "redirect to the next page on clicking agree and continue and not audit TCMP when TypeOfBenefit is Defined" in {
+      reset(mockAuditService)
+
+      val result = controller(tcmpAuditDataUa(TypeOfBenefits.Defined).dataRetrievalAction).onClickAgree()(fakeRequest)
+
+      whenReady(result) {
+        response =>
+          response.header.status mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(onwardRoute.url)
+          verify(mockAuditService, times(0)).sendExtendedEvent(any())(any(), any())
+      }
+    }
+
+    "send an email on clicking agree and continue" which {
+      "fetches from Get PSA Minimal Details" in {
+
+        reset(mockEmailConnector)
+
+        when(mockEmailConnector.sendEmail(eqTo("test@test.com"), eqTo("pods_scheme_register"), any(), any())(any(), any()))
+          .thenReturn(Future.successful(EmailSent))
+
+        whenReady(controller(nonDormantCompany, fakeEmailConnector = mockEmailConnector).onClickAgree()(fakeRequest)) { _ =>
+
+          verify(mockEmailConnector, times(1)).sendEmail(
+            eqTo("test@test.com"),
+            eqTo("pods_scheme_register"),
+            eqTo(Map("srn" -> "S12345 67890", "psaName" -> "psa name")),
+            eqTo(psaId)
+          )(any(), any())
+
+        }
+      }
+    }
+
+    "redirect to Session Expired" when {
+      "no existing data is found" when {
+        "GET" in {
+          val result = controller(dontGetAnyData).onPageLoad()(fakeRequest)
+
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+        }
+        "POST" in {
+          val result = controller(dontGetAnyData).onClickAgree()(fakeRequest)
+
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+        }
+      }
+    }
+
+  }
+
+}
+
+object DeclarationControllerSpec extends ControllerSpecBase with MockitoSugar with DataCompletionHelper {
+  private def onwardRoute: Call = controllers.routes.IndexController.onPageLoad()
+
+  private val formProvider = new DeclarationFormProvider()
+  private val form = formProvider()
+  private val href = controllers.register.routes.DeclarationController.onClickAgree()
+  val psaId = PsaId("A0000000")
+
+  private val mockHsTaskListHelperRegistration = mock[HsTaskListHelperRegistration]
+  private val mockAuditService = mock[AuditService]
+
+  private val view = injector.instanceOf[declaration]
+
+  private def uaWithBasicData: UserAnswers =
+    setCompleteBeforeYouStart(
+      isComplete = true,
+      setCompleteMembers(
+        isComplete = true,
+        setCompleteBank(
+          isComplete = true,
+          setCompleteBenefits(
+            isComplete = true,
+            setCompleteEstIndividual(0, UserAnswers())
+          )
+        )
+      )
+    )
+      .set(HaveAnyTrusteesId)(false).asOpt.value
+
+  private def controller(dataRetrievalAction: DataRetrievalAction,
+                         fakeEmailConnector: EmailConnector = fakeEmailConnector
+                        ): DeclarationController =
+    new DeclarationController(
+      frontendAppConfig,
+      messagesApi,
+      FakeUserAnswersCacheConnector,
+      new FakeNavigator(onwardRoute),
+      FakeAuthAction,
+      dataRetrievalAction,
+      new DataRequiredActionImpl,
+      fakePensionsSchemeConnector,
+      fakeEmailConnector,
+      fakeMinimalPsaConnector,
+      controllerComponents,
+      mockHsTaskListHelperRegistration,
+      view,
+      mockAuditService
+    )
+
+  private def viewAsString(form: Form[_] = form, isCompany: Boolean, isDormant: Boolean,
+                           showMasterTrustDeclaration: Boolean = false, hasWorkingKnowledge: Boolean = false): String =
+    view(
+      isCompany,
+      isDormant,
+      showMasterTrustDeclaration,
+      hasWorkingKnowledge,
+      Some("Test Scheme"),
+      href
+    )(fakeRequest, messages).toString
+
+  private def individualEst: DataRetrievalAction = {
+    setCompleteWorkingKnowledge(isComplete = true, uaWithBasicData)
+      .set(identifiers.DeclarationDutiesId)(false).asOpt
+      .value
+      .dataRetrievalAction
+  }
+
+  private def dataWithMasterTrust: DataRetrievalAction = {
+    setCompleteWorkingKnowledge(isComplete = true, uaWithBasicData)
+      .set(identifiers.DeclarationDutiesId)(false).asOpt
+      .value.schemeType(SchemeType.MasterTrust).set(HaveAnyTrusteesId)(false).asOpt.value
+      .dataRetrievalAction
+  }
+
+  private def tcmpAuditDataUa(typeOfBenefit: TypeOfBenefits): UserAnswers =
+    setCompleteWorkingKnowledge(
+      isComplete = true,
+      ua = setCompleteEstCompany(1, uaWithBasicData)
+    )
+      .set(identifiers.DeclarationDutiesId)(false)
+      .asOpt
+      .value
+      .establisherCompanyDormant(1, DeclarationDormant.No)
+      .set(MoneyPurchaseBenefitsId)(Seq(MoneyPurchaseBenefits.Collective))
+      .asOpt
+      .value
+      .set(TypeOfBenefitsId)(typeOfBenefit)
+      .asOpt
+      .value
+      .set(DeclarationId)(true)
+      .asOpt
+      .value
+
+  private val nonDormantCompany: DataRetrievalAction =
+    setCompleteWorkingKnowledge(
+      isComplete = true,
+      ua = setCompleteEstCompany(1, uaWithBasicData)
+    )
+      .set(identifiers.DeclarationDutiesId)(false)
+      .asOpt
+      .value
+      .establisherCompanyDormant(1, DeclarationDormant.No)
+      .dataRetrievalAction
+
+  private val dormantCompany: DataRetrievalAction = {
+    setCompleteWorkingKnowledge(
+      isComplete = true, setCompleteEstCompany(1, uaWithBasicData))
+      .set(identifiers.DeclarationDutiesId)(false).asOpt
+      .value.establisherCompanyDormant(1, DeclarationDormant.Yes).dataRetrievalAction
+  }
+
+  private val mockEmailConnector = mock[EmailConnector]
+
+  private val validSchemeSubmissionResponse = SchemeSubmissionResponse("S1234567890")
+
+  private val fakePensionsSchemeConnector = new PensionsSchemeConnector {
+    override def registerScheme
+    (answers: UserAnswers, psaId: String)
+    (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Either[HttpResponse, SchemeSubmissionResponse]] = {
+      Future.successful(Right(validSchemeSubmissionResponse))
+    }
+
+    override def updateSchemeDetails(psaId: String, pstr: String, answers: UserAnswers)(
+      implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Either[HttpResponse, Unit]] = ???
+
+    override def checkForAssociation(psaId: String, srn: String)
+                                    (implicit headerCarrier: HeaderCarrier,
+                                     ec: ExecutionContext, request: RequestHeader): Future[Either[HttpResponse, Boolean]] = ???
+  }
+
+  private val fakeEmailConnector = new EmailConnector {
+    override def sendEmail
+    (emailAddress: String, templateName: String, params: Map[String, String] = Map.empty, psaId: PsaId)
+    (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[EmailStatus] = {
+      Future.successful(EmailSent)
+    }
+  }
+
+  private val fakeMinimalPsaConnector = new MinimalPsaConnector {
+    override def getMinimalFlags(psaId: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[PSAMinimalFlags] =
+      Future.successful(PSAMinimalFlags(isSuspended = true, isDeceased = false))
+
+    override def getMinimalPsaDetails(psaId: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[MinimalPSA] =
+      Future.successful(MinimalPSA("test@test.com", isPsaSuspended = true, Some("psa name"), None))
+
+    override def getPsaNameFromPsaID(psaId: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Option[String]] =
+      Future.successful(Some("psa name"))
+  }
+}
