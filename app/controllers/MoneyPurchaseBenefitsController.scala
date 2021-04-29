41eb5e0af372995b930273829993874d9a0b9a60
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2021-04-29 09:50:48 +0100

    PODS-5354: Removed incorrect logic for exisitng answers

--- app/controllers/MoneyPurchaseBenefitsController.scala
+++ app/controllers/MoneyPurchaseBenefitsController.scala
@@ -19,12 +19,10 @@ package controllers
 import controllers.actions._
 import forms.MoneyPurchaseBenefitsFormProvider
 import identifiers.{MoneyPurchaseBenefitsId, TcmpChangedId}
-import models.requests.DataRequest
 import models.{Mode, MoneyPurchaseBenefits}
 import navigators.Navigator
 import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.libs.json.{JsError, JsResultException, JsSuccess}
 import play.api.mvc.{Action, AnyContent, Call, MessagesControllerComponents}
 import services.UserAnswersService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
@@ -56,46 +54,13 @@ class MoneyPurchaseBenefitsController @Inject()(
 
   private def form: Form[MoneyPurchaseBenefits] = formProvider()
 
-  private def convertTcmpArrToStr(implicit request: DataRequest[AnyContent]): Form[MoneyPurchaseBenefits] = {
-    request.userAnswers.get(MoneyPurchaseBenefitsId) match {
-      case Some(_) =>
-        (request.userAnswers.json \ "moneyPurchaseBenefits").validate[Seq[MoneyPurchaseBenefits]] match {
-          case JsSuccess(mpb, _) =>
-            mpb match {
-              case Seq(MoneyPurchaseBenefits.Collective) =>
-                form.fill(MoneyPurchaseBenefits.Collective)
-              case Seq(MoneyPurchaseBenefits.Other) =>
-                form.fill(MoneyPurchaseBenefits.Other)
-              case Seq(MoneyPurchaseBenefits.CashBalance) =>
-                form.fill(MoneyPurchaseBenefits.CashBalance)
-              case Seq(MoneyPurchaseBenefits.Other, MoneyPurchaseBenefits.CashBalance) =>
-                form.fill(MoneyPurchaseBenefits.MixtureCashBalanceAndOther)
-              case Seq(MoneyPurchaseBenefits.Collective, MoneyPurchaseBenefits.Other, MoneyPurchaseBenefits.CashBalance) =>
-                form.fill(MoneyPurchaseBenefits.MixtureCollectiveAndCashBalanceAndOrOther)
-              case _ =>
-                throw MoneyPurchaseBenefitsUnrecognisedException
-            }
-          case JsError(err1) =>
-            (request.userAnswers.json \ "moneyPurchaseBenefits").validate[MoneyPurchaseBenefits] match {
-              case JsSuccess(mpb, _) =>
-                form.fill(mpb)
-              case JsError(err2) =>
-                throw JsResultException(err1 ++ err2)
-            }
-        }
-      case _ =>
-        form
-    }
-  }
-
-  object MoneyPurchaseBenefitsUnrecognisedException extends Exception("Money purchase benefit not recognised")
 
   def onPageLoad(mode: Mode, srn: Option[String]): Action[AnyContent] =
     (authenticate() andThen getData(mode, srn) andThen allowAccess(srn) andThen requireData).async {
       implicit request =>
         Future.successful(Ok(
           view(
-            form = convertTcmpArrToStr,
+            form = request.userAnswers.get(MoneyPurchaseBenefitsId).fold(form)(form.fill),
             mode = mode,
             schemeName = existingSchemeName,
             postCall = postCall(mode, srn),
