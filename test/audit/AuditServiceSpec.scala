13117087f39d27b066541f0333ad84cb1afb5744
Emma Rawsthorne <27211889+emrawsy@users.noreply.github.com>
2021-06-01 12:03:19 +0100

    PODS-5480 updating dependencies

--- test/audit/AuditServiceSpec.scala
+++ test/audit/AuditServiceSpec.scala
@@ -24,7 +24,7 @@ import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.audit.http.config.AuditingConfig
-import uk.gov.hmrc.play.audit.http.connector.{AuditConnector, AuditResult}
+import uk.gov.hmrc.play.audit.http.connector._
 import uk.gov.hmrc.play.audit.model.DataEvent
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -89,9 +89,13 @@ object FakeAuditConnector extends AuditConnector {
 
   def lastSentEvent: DataEvent = sentEvent
 
-  override def materializer: Materializer = ???
+  def materializer: Materializer = ???
 
-  override def lifecycle: ApplicationLifecycle = ???
+  def lifecycle: ApplicationLifecycle = ???
+
+  def auditChannel: AuditChannel = ???
+
+  def auditCounter: AuditCounter = ???
 }
 
 case class TestAuditEvent(payload: String) extends AuditEvent {
