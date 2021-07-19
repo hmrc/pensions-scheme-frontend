510b99b812b9be5db87f75eb1833e4f096ea1267
Emma Rawsthorne <27211889+emrawsy@users.noreply.github.com>
2021-07-19 08:31:05 +0100

    PODS-5496 Made regex less verbose

--- app/forms/mappings/Constraints.scala
+++ app/forms/mappings/Constraints.scala
@@ -28,7 +28,7 @@ trait Constraints {
   val regexPostcode = """^[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}$"""
   val regexPostCodeNonUk = """^([0-9]+-)*[0-9]+$"""
   val regexSortCode: String = """\d{6,}""".r.toString()
-  val regexUtr = """^([kK]{0,1}[0-9]{10,10})$|^([0-9]{10,10}[kK]{0,1})$|^([kK]{0,1}[0-9]{13,13})$|^([0-9]{13,13}[kK]{0,1})$"""
+  val regexUtr = """^([kK]{0,1}\d{10})$|^(\d{10}[kK]{0,1})$|^([kK]{0,1}\d{13})$|^(\d{13}[kK]{0,1})$"""
   val regexName = """^[a-zA-Z &`\-\'\.^]{1,35}$"""
   val regexPersonOrOrganisationName =   """^[a-zA-Z\u00C0-\u00FF '‘’\u2014\u2013\u2010\u002d]{1,107}"""
   val regexUserResearch = """^[a-zA-Z\u00C0-\u00FF '‘’\u2014\u2013\u2010\u002d]{1,160}$"""
