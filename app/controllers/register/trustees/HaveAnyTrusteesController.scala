03bb6de9f0d9a9d916ed0f1db240582dc4618a0e
Shehzad Ismail <shehzad.ismail@atos.net>
2022-09-20 11:47:37 +0100

    [PODS-7532: Added new controller and view for trustees]

---
+++ app/controllers/register/trustees/HaveAnyTrusteesController.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.trustees
+
+import config.FrontendAppConfig
+import connectors.UserAnswersCacheConnector
+import controllers.Retrievals
+import controllers.actions._
+import forms.register.trustees.HaveAnyTrusteesFormProvider
+import identifiers.{HaveAnyTrusteesId, SchemeNameId}
+import models.Mode
+import navigators.Navigator
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.UserAnswers
+import utils.annotations.BeforeYouStart
+import views.html.register.trustees.haveAnyTrustees
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class HaveAnyTrusteesController @Inject()(
+                                           override val messagesApi: MessagesApi,
+                                           dataCacheConnector: UserAnswersCacheConnector,
+                                           @BeforeYouStart navigator: Navigator,
+                                           authenticate: AuthAction,
+                                           getData: DataRetrievalAction,
+                                           requireData: DataRequiredAction,
+                                           formProvider: HaveAnyTrusteesFormProvider,
+                                           val controllerComponents: MessagesControllerComponents,
+                                           val view: haveAnyTrustees
+                                         )(implicit val executionContext: ExecutionContext) extends
+  FrontendBaseController with I18nSupport with Retrievals {
+
+  private val form: Form[Boolean] = formProvider()
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (authenticate() andThen getData() andThen requireData).async {
+    implicit request =>
+      SchemeNameId.retrieve.right.map { schemeName =>
+        val preparedForm = request.userAnswers.get(HaveAnyTrusteesId) match {
+          case None => form
+          case Some(value) => form.fill(value)
+        }
+        Future.successful(Ok(view(preparedForm, mode, schemeName)))
+      }
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (authenticate() andThen getData() andThen requireData).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          SchemeNameId.retrieve.right.map { schemeName =>
+            Future.successful(BadRequest(view(formWithErrors, mode, schemeName)))
+          },
+        value =>
+          dataCacheConnector.save(request.externalId, HaveAnyTrusteesId, value).map(cacheMap =>
+            Redirect(navigator.nextPage(HaveAnyTrusteesId, mode, UserAnswers(cacheMap))))
+      )
+  }
+}
