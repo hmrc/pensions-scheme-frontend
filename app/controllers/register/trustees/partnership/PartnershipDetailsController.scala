fc4fc45c4f7f4a0ac639772832dbbf7e99e05c0a
Mary1-5 <marymansha15@gmail.com>
2022-09-26 16:02:37 +0100

    Adding temptoggledef

--- app/controllers/register/trustees/partnership/PartnershipDetailsController.scala
+++ app/controllers/register/trustees/partnership/PartnershipDetailsController.scala
@@ -21,13 +21,17 @@ import controllers.Retrievals
 import controllers.actions.{AllowAccessActionProvider, AuthAction, DataRequiredAction, DataRetrievalAction}
 import forms.register.PartnershipDetailsFormProvider
 import identifiers.register.trustees.partnership.PartnershipDetailsId
+import models.FeatureToggleName.SchemeRegistration
+import models.requests.DataRequest
+
 import javax.inject.Inject
-import models.{Index, Mode}
+import models.{FeatureToggleName, Index, Mode}
 import navigators.Navigator
 import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.{JsObject, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import services.UserAnswersService
+import services.{FeatureToggleService, UserAnswersService}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import utils.{Enumerable, UserAnswers}
 import views.html.register.trustees.partnership.partnershipDetails
@@ -45,7 +49,8 @@ class PartnershipDetailsController @Inject()(
                                               requireData: DataRequiredAction,
                                               formProvider: PartnershipDetailsFormProvider,
                                               val controllerComponents: MessagesControllerComponents,
-                                              val view: partnershipDetails
+                                              val view: partnershipDetails,
+                                              featureToggleService: FeatureToggleService
                                             )(implicit val executionContext: ExecutionContext) extends
   FrontendBaseController with Retrievals with I18nSupport with Enumerable.Implicits {
   private val form = formProvider()
@@ -76,5 +81,14 @@ class PartnershipDetailsController @Inject()(
               }
           }
       )
+  }
+
+  //TODO: Remove whole method once toggle is removed
+  private def tempToggleAmend(ua: UserAnswers)(implicit request: DataRequest[AnyContent]): Future[UserAnswers] = {
+    featureToggleService.get(FeatureToggleName.SchemeRegistration).map(_.isEnabled).map { toggleValue =>
+      val uaAsJsObject = ua.json.as[JsObject]
+      val updatedJson = uaAsJsObject ++ Json.obj(SchemeRegistration.asString -> toggleValue)
+      UserAnswers(updatedJson)
+    }
   }
 }
