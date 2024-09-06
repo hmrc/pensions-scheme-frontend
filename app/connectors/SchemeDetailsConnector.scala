97fda42c69bb1938597a0730b043e67733f10edf
srinivasulumunagalahmrc <srinivasulu.munagala@digital.hmrc.gov.uk>
2024-09-06 09:15:12 +0100

    PODS-9662 controller changes with srn as  optional in DataRetrievalAction

--- app/connectors/SchemeDetailsConnector.scala
+++ app/connectors/SchemeDetailsConnector.scala
@@ -34,8 +34,6 @@ trait SchemeDetailsConnector {
   def getSchemeDetails(psaId: String, schemeIdType: String, idNumber: String, refreshData: Option[Boolean] = None)
                       (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[UserAnswers]
 
-  def isPsaAssociated(psaOrPspId: String, idType: String, srn: SchemeReferenceNumber)
-                     (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Option[Boolean]]
 
   def getPspSchemeDetails(pspId: String, srn: SchemeReferenceNumber, refreshData: Option[Boolean] = None)
                          (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[UserAnswers]
@@ -66,38 +64,6 @@ class SchemeDetailsConnectorImpl @Inject()(http: HttpClient, config: FrontendApp
       }
     } andThen {
       case Failure(t: Throwable) => logger.warn("Unable to get scheme details", t)
-    }
-  }
-
-  override def isPsaAssociated(
-                                psaOrPspId: String,
-                                idType: String,
-                                srn: SchemeReferenceNumber
-                              )(
-                                implicit hc: HeaderCarrier,
-                                ec: ExecutionContext
-                              ): Future[Option[Boolean]] = {
-
-    val isSchemeAssociatedUrl = config.isSchemeAssociatedUrl
-    val idHeader = if(idType == "psa") "psaId" -> psaOrPspId else "pspId" -> psaOrPspId
-    val schemeHc =
-      hc.withExtraHeaders(
-        idHeader,
-        "schemeReferenceNumber" -> srn
-      )
-    http.GET[HttpResponse](isSchemeAssociatedUrl)(implicitly, schemeHc, implicitly).map { response =>
-      response.status match {
-        case OK =>
-          response.json.validate[Boolean] match {
-            case JsSuccess(isDataChanged, _) => Some(isDataChanged)
-            case JsError(errors) =>
-              logger.warn(s"Unable to de-serialise the response $errors")
-              None
-          }
-        case _ => handleErrorResponse("GET", isSchemeAssociatedUrl)(response)
-      }
-    } andThen {
-      case Failure(t: Throwable) => logger.warn("Unable to get  if this psaOrPspId is associated with the scheme details", t)
     }
   }
 
