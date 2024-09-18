338111daf721f28021e7d2a24720e1c93fd04111
srinivasulumunagalahmrc <srinivasulu.munagala@digital.hmrc.gov.uk>
2024-09-18 10:23:56 +0100

    Revert "PODS-9662 simple change of making srn work"
    This reverts commit 94bdab49aa5fa5be6e4159cbef55ffb5c19cbde4.

--- app/models/SchemeReferenceNumber.scala
+++ app/models/SchemeReferenceNumber.scala
@@ -25,6 +25,7 @@ import scala.util.matching.Regex
 case class SchemeReferenceNumber(id: String){
   implicit def optionPathBindable(implicit stringBinder: PathBindable[String]): PathBindable[Option[SchemeReferenceNumber]] = new PathBindable[Option[SchemeReferenceNumber]] {
     override def bind(key: String, value: String): Either[String, Option[SchemeReferenceNumber]] = {
+      println(s"************in optionPathBindable path bind $key $value")
       if (value.isEmpty) {
         Right(None)
       } else {
@@ -36,6 +37,7 @@ case class SchemeReferenceNumber(id: String){
     }
 
     override def unbind(key: String, srnOpt: Option[SchemeReferenceNumber]): String = {
+      println(s"************in optionPathBindable unbind $key $srnOpt")
       srnOpt.map(_.id).getOrElse("")
     }
   }
@@ -52,6 +54,7 @@ object SchemeReferenceNumber {
     val regexSRN: Regex = "^S[0-9]{10}$".r
 
     override def bind(key: String, value: String): Either[String, SchemeReferenceNumber] = {
+      println(s"************in srnPathBindable path bind $key $value")
 
       val pattern = """SchemeReferenceNumber\((.*?)\)""".r
 
@@ -64,11 +67,13 @@ object SchemeReferenceNumber {
         case Right(srn@regexSRN(_*)) =>
           Right(SchemeReferenceNumber(srn))
         case x =>
+          println(s"what is this srn!!! $x")
           Left("SchemeReferenceNumber binding failed")
       }
     }
 
     override def unbind(key: String, value: SchemeReferenceNumber): String = {
+      println(s"************in srnPathBindable path unbind $key $value")
       stringBinder.unbind(key, value.id)
     }
   }
@@ -80,6 +85,7 @@ object SchemeReferenceNumber {
   implicit def queryBindable(implicit stringBinder: QueryStringBindable[String]): QueryStringBindable[SchemeReferenceNumber] =
     new QueryStringBindable[SchemeReferenceNumber] {
       override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, SchemeReferenceNumber]] = {
+        println(s"************in queryBindable bind $key $params")
 
         stringBinder.bind(key, params) match {
           case Some(Right(id)) => Some(Right(SchemeReferenceNumber(id)))
@@ -89,6 +95,7 @@ object SchemeReferenceNumber {
       }
 
       override def unbind(key: String, value: SchemeReferenceNumber): String = {
+        println(s"************in queryBindable unbind $key $value")
 
         stringBinder.unbind(key, value.id)
       }
@@ -100,6 +107,7 @@ object SchemeReferenceNumber {
   implicit def optionQueryBindable(implicit schemeRefBinder: QueryStringBindable[SchemeReferenceNumber]): QueryStringBindable[Option[SchemeReferenceNumber]] =
     new QueryStringBindable[Option[SchemeReferenceNumber]] {
       override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, Option[SchemeReferenceNumber]]] = {
+        println(s"in optionQueryBindable bind $key $params")
 
         params.get(key) match {
           case Some(Seq("")) => Some(Right(None))
@@ -114,6 +122,7 @@ object SchemeReferenceNumber {
       }
 
       override def unbind(key: String, value: Option[SchemeReferenceNumber]): String = {
+        println(s"in optionQueryBindable unbind $key $value")
 
         value.map(schemeRefBinder.unbind(key, _)).getOrElse(s"$key=noSRN")
       }
@@ -121,6 +130,13 @@ object SchemeReferenceNumber {
 
   implicit def schemeReferenceNumberToString(srn: SchemeReferenceNumber): String =
     srn.id
+
+//  implicit def stringToSchemeReferenceNumber(srn: SchemeReferenceNumber): SchemeReferenceNumber = {
+//    println(s"************* stringToSchemeReferenceNumber $srn")
+//    SchemeReferenceNumber(srn)
+//  }
+
+  case class InvalidSchemeReferenceNumberException() extends Exception
 
   implicit val format: OFormat[SchemeReferenceNumber] = Json.format[SchemeReferenceNumber]
 
