08bf73dcab8d7201074ff897303efe4caa3c7592
nonebula <147166151+nonebula@users.noreply.github.com>
2024-10-21 09:54:10 +0100

    PODS-9935 - dependencies updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,17 +6,17 @@ object AppDependencies {
   private val bootstrapVersion = "9.5.0"
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"                   %%  "play-conditional-form-mapping-play-30" % "2.0.0",
-    "uk.gov.hmrc"                   %%  "bootstrap-frontend-play-30"    % bootstrapVersion,
-    "uk.gov.hmrc"                   %%  "domain-play-30"                % "9.0.0",
-    "com.typesafe.play"             %%  "play-json-joda"                % "2.10.6",
-    "com.google.inject.extensions"  %   "guice-multibindings"           % "4.2.3",
-    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"    % "10.5.0",
-    "org.owasp.encoder"             %   "encoder"                       % "1.2.3"
+    "uk.gov.hmrc"                   %%  "play-conditional-form-mapping-play-30" % "3.2.0",
+    "uk.gov.hmrc"                   %%  "bootstrap-frontend-play-30"            % bootstrapVersion,
+    "uk.gov.hmrc"                   %% "domain"                                 % "8.1.0-play-28",
+    "com.typesafe.play"             %%  "play-json-joda"                        % "2.10.6",
+    "com.google.inject.extensions"  %   "guice-multibindings"                   % "4.2.3",
+    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"            % "10.13.0",
+    "org.owasp.encoder"             %   "encoder"                               % "1.2.3"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-30" % bootstrapVersion         % Test,
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-30" % bootstrapVersion    % Test,
     "org.scalatest"                %% "scalatest"              % "3.2.19"            % Test,
     "org.scalatestplus"            %% "scalacheck-1-17"        % "3.2.18.0"          % Test,
     "org.scalatestplus"            %% "mockito-4-6"            % "3.2.15.0"          % Test,
