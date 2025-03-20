e5170cbf2d549b936364198b1d4c91ada6e35b3a
Pavel Vjalicin <pavelvjalicin@users.noreply.github.com>
2025-03-20 04:24:57 +0000

    PODS-9915: bump versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object AppDependencies {
 
   import play.sbt.PlayImport._
-  private val bootstrapVersion = "9.7.0"
+  private val bootstrapVersion = "9.11.0"
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"                   %%  "play-conditional-form-mapping-play-30" % "3.2.0",
     "uk.gov.hmrc"                   %%  "bootstrap-frontend-play-30"            % bootstrapVersion,
     "com.google.inject.extensions"  %   "guice-multibindings"                   % "4.2.3",
-    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"            % "11.3.0",
+    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"            % "11.12.0",
     "uk.gov.hmrc"                   %%  "domain-play-30"                        % "10.0.0",
     "org.owasp.encoder"             %   "encoder"                               % "1.3.1"
   )
