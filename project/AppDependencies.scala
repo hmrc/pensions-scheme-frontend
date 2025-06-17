56b119f4ce6b3e4f1f3db886fbb229a69dcb0ace
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2025-06-17 09:48:13 +0100

    DDCNL-10301: PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 object AppDependencies {
 
   import play.sbt.PlayImport.*
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "9.13.0"
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"                   %%  "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"                   %%  "bootstrap-frontend-play-30"            % bootstrapVersion,
     "com.google.inject.extensions"  %   "guice-multibindings"                   % "4.2.3",
-    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"            % "12.1.0",
+    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"            % "12.5.0",
     "uk.gov.hmrc"                   %%  "domain-play-30"                        % "11.0.0",
     "org.owasp.encoder"             %   "encoder"                               % "1.3.1"
   )
