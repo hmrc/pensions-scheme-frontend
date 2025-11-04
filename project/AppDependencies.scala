44a9cd333afad72cca303f8dd0f8eca2aa6a4a4f
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2025-11-04 11:12:46 +0000

    DDCNL-11260: Update bootstrap to 10.4.0 as per recent tech blog post … (#1309)
    * DDCNL-11260: Update bootstrap to 10.4.0 as per recent tech blog post around secure cookies
    
    * DDCNL-11260: Deps update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 object AppDependencies {
 
   import play.sbt.PlayImport.*
-  private val bootstrapVersion = "10.3.0"
+  private val bootstrapVersion = "10.4.0"
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"                   %%  "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"                   %%  "bootstrap-frontend-play-30"            % bootstrapVersion,
     "com.google.inject.extensions"  %   "guice-multibindings"                   % "4.2.3",
-    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"            % "12.18.0",
+    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"            % "12.19.0",
     "uk.gov.hmrc"                   %%  "domain-play-30"                        % "13.0.0",
     "org.owasp.encoder"             %   "encoder"                               % "1.3.1"
   )
